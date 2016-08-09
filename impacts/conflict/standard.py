6bd55337af5707b28f3cb9f32f5c2fa703e58209
James Rising <jarising@gmail.com>
2016-08-09 07:17:32 -0700

    conflict testing

--- impacts/conflict/standard.py
+++ impacts/conflict/standard.py
@@ -6,21 +6,23 @@ Precipitation is as a difference of polynomials (sum_k P^k - mean P^k)
 """
 
 from openest.generate.stdlib import *
-from adaptation import csvvfile
+from adaptation import csvvfile, interpolate
 
-def prepare_raw_csvv(csvvpath, pvals):
+def prepare_raw_csvv(csvvpath, qvals):
     data = csvvfile.read(csvvpath)
 
-    tcoeff = LinearInterpolatedValue(extractValues(data, [0]), pvals)
-    p3coeffs = LinearInterpolatedPolynomial(extractValues(data, range(1, 4)), pvals)
+    tcoeff = InterpolatedLinearCurve(qvals['temperature'], *csvvfile.extract_values(data, [0]))
+    #p3coeffs = LinearInterpolatedPolynomial(extract_values(data, range(1, 4)), qvals['precipitation'])
+    
+    teffect = InstaZScore(tcoeff)
 
-    teffect = ApplyEstimated(tcoeff, 'z-score', 'delta rate',
-        Transform(WeatherVariable('tas'), 'C', 'z-score',
-                  lambda tas: (tas - climate_mean('tas')) / climate_sdev('tas'))
-    p3effect = ApplyEstimated(p3coeffs, 'anomaly poly', 'delta rate',
-        Transform(WeatherVariable('prcp'), 'mm/day', 'anomaly poly',
-                  lambda prcp: [prcp - climate_mean('prcp'),
-                                prcp**2 - climate_mean('prcp')**2,
-                                prcp**3 - climate_mean('prcp')**3])
+    #teffect = ApplyEstimated(tcoeff, 'z-score', 'delta rate',
+    #    RegionalTransform(WeatherVariable('tas'), 'C', 'z-score',
+    #                      lambda tas, ii: (tas - climate_mean('tas', ii)) / climate_sdev('tas', ii))
+    #p3effect = ApplyEstimated(p3coeffs, 'anomaly poly', 'delta rate',
+    #    Transform(WeatherVariable('prcp'), 'mm/day', 'anomaly poly',
+    #              lambda prcp: [prcp - climate_mean('prcp'),
+    #                            prcp**2 - climate_mean('prcp')**2,
+    #                            prcp**3 - climate_mean('prcp')**3])
 
-    return Sum(teffect, p3effect)
+    return teffect #Sum(teffect, p3effect)
