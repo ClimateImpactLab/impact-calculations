181044a310afe1a99c2a55774a46524712da13ce
James Rising <jarising@gmail.com>
2016-08-09 19:27:17 -0700

    debug 2

--- shortterm/standard.py
+++ shortterm/standard.py
@@ -1,6 +1,10 @@
 from impacts.conflict import standard
+from impacts.weather import MultivariateHistoricalWeatherBundle
 
 def produce(targetdir, weatherbundle, qvals, do_only=None):
+    historicalbundle = MultivariateHistoricalWeatherBundle("/shares/gcp/BCSD/grid2reg/cmip5/historical/CCSM4/{0}/{0}_day_aggregated_historical_r1i1p1_CCSM4_{1}.nc", 1991, 2005, ['pr', 'tas'])
+    econbundle = 
+
     if do_only is None or do_only == 'acp':
         # ACP response
         calculation, dependencies = caller.call_prepare('impacts.conflict.ACRA_violentcrime', weatherbundle, qvals['ACRA_violentcrime'])
