d6f54498a2968d4b9df14811fbe52df8c949b9c3
James Rising <jarising@gmail.com>
2016-08-10 10:46:45 -0700

    debug 6

--- shortterm/weather.py
+++ shortterm/weather.py
@@ -1,6 +1,7 @@
 import numpy as np
 from netCDF4 import Dataset
 from scipy.stats import norm
+from impacts.weather import WeatherBundle
 
 temp_path = "/shares/gcp/IRI/tas_aggregated_quantiles_2012-2016.nc"
 prcp_path = "/shares/gcp/IRI/prcp_aggregated_quantiles_2012-2016.nc"
@@ -24,17 +25,31 @@ def readncdf_allpred(filepath, variable, lead):
     rootgrp.close()
 
     for month in range(alldata.shape[0]):
-        yield rootgrp.variables[variable][month, lead, :]
+        yield alldata[month, lead, :]
 
-class FirstForecastBundle(object):
-    def __init__(self, filepath):
+class FirstForecastBundle(WeatherBundle):
+    def __init__(self, filepath, hierarchy='hierarchy.csv'):
+        super(FirstForecastBundle, self).__init__(hierarchy)
         self.filepath = filepath
+        self.dependencies = [filepath]
+
+        self.load_regions()
+        self.load_metainfo(self.filepath, 'mean')
+
+    def get_months(self):
+        rootgrp = Dataset(self.filepath, 'r', format='NETCDF4')
+        months = rootgrp.variables['S'][:]
+        months_title = rootgrp.variables['S'].units
+        rootgrp.close()
+        
+        return months, months_title
 
     def monthbundles(self, qval, maxyear=np.inf):
+        months, months_title = self.get_months()
         meansgen = readncdf_allpred(self.filepath, "mean", 0)
         sdevsgen = readncdf_allpred(self.filepath, "stddev", 0)
-        for means in meansgen:
-            yield norm.ppf(qval, means, sdevsgen.next())
+        for month in months:
+            yield month, norm.ppf(qval, meansgen.next(), sdevsgen.next())
 
 if __name__ == '__main__':
     print np.mean(readncdf_lastpred(temp_path, "mean", 0))
@@ -42,4 +57,7 @@ if __name__ == '__main__':
 
     bundle = FirstForecastBundle(temp_path)
     gener = bundle.monthbundles(.5)
-    print np.mean(gener.next())
+    print np.mean(gener.next()[1])
+
+    for monthvals in readncdf_allpred(temp_path, 'mean', 0):
+        print monthvals[1000]
