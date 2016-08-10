76600e7e78fec0ffba78f3808dd32e4b5e7549e0
James Rising <jarising@gmail.com>
2016-08-10 09:44:58 -0400

    prep predictors

--- adaptation/econmodel.py
+++ adaptation/econmodel.py
@@ -45,6 +45,31 @@ class SSPEconomicModel(object):
         for region in pop_baseline.keys():
             yield region, gdppc_baseline.get(region, None), self.densities.get(region, None)
 
+    def baseline_prepared(self, maxbaseline, numeconyears, func):
+        econ_predictors = {} # {region: ([gdppcs], density)}
+        allmeans_gdppcs = []
+        allmeans_density = []
+        for region, gdppcs, density in self.baseline_values(maxbaseline): # baseline through maxbaseline
+            if gdppcs is None or density is None:
+                if density is not None:
+                    density = func([density])
+                if gdppcs is not None:
+                    gdppcs = func(gdppcs[-numeconyears:])
+                econ_predictors[region] = [gdppcs, density]
+            else:
+                allmeans_gdppcs.append(np.mean(gdppcs[-numeconyears:]))
+                allmeans_density.append(density)
+                econ_predictors[region] = [func(gdppcs[-numeconyears:]), func([density])]
+
+        econ_predictors['mean'] = [np.mean(allmeans_gdppcs), np.mean(allmeans_density)] # don't use mean density-- all should have
+        for region in econ_predictors:
+            if econ_predictors[region][0] is None:
+                econ_predictors[region][0] = func([econ_predictors['mean'][0]])
+            if econ_predictors[region][1] is None:
+                econ_predictors[region][1] = func([econ_predictors['mean'][1]])
+
+        return econ_predictors
+
     def get_gdppc_year(self, region, year):
         if region not in self.gdppc_future_years:
             return None
