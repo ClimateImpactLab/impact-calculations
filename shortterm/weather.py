8d19c40bab342291e20bd0cbbc46bc63d46ae38b
James Rising <jarising@gmail.com>
2016-08-13 07:10:51 -0700

    conflict v. dampwood

--- shortterm/weather.py
+++ shortterm/weather.py
@@ -5,6 +5,8 @@ from impacts.weather import WeatherBundle
 
 temp_path = "/shares/gcp/IRI/tas_aggregated_quantiles_2012-2016.nc"
 prcp_path = "/shares/gcp/IRI/prcp_aggregated_quantiles_2012-2016.nc"
+temp_climate_path = "/shares/gcp/IRI/tas_aggregated_climatology_1981-2010.nc"
+prcp_climate_path = "/shares/gcp/IRI/prcp_aggregated_climatology_1981-2010.nc"
 
 def readncdf_lastpred(filepath, variable, lead):
     """
@@ -50,6 +52,34 @@ class FirstForecastBundle(WeatherBundle):
         sdevsgen = readncdf_allpred(self.filepath, "stddev", 0)
         for month in months:
             yield month, norm.ppf(qval, meansgen.next(), sdevsgen.next())
+
+class CombinedBundle(WeatherBundle):
+    def __init__(self, bundles, hierarchy='hierarchy.csv'):
+        super(CombinedBundle, self).__init__(hierarchy)
+        self.bundles = bundles
+        self.dependencies = set()
+        for bundle in bundles:
+            self.dependencies.update(bundle.dependencies)
+        self.dependencies = list(self.dependencies)
+
+        self.load_regions()
+        self.version = bundles[0].version
+        self.units = [bundle.units for bundle in bundles]
+
+    def get_months(self):
+        return self.bundles[0].get_months()
+
+    def monthbundles(self, qval, maxyear=np.inf):
+        months, months_title = self.get_months()
+        iterators = [bundle.monthbundles(qval) for bundle in self.bundles]
+        for month in months:
+            results = []
+            for iterator in iterators:
+                monthii, result = iterator.next()
+                assert month == monthii
+                results.append(result)
+
+            yield month, np.array(results)
 
 if __name__ == '__main__':
     print np.mean(readncdf_lastpred(temp_path, "mean", 0))
