91cfbf7058256d5f13d2ef4c519fffd627381a5d
James Rising <jarising@gmail.com>
2016-08-18 14:25:48 -0700

    add comments

--- climate/reader.py
+++ climate/reader.py
@@ -25,6 +25,8 @@ class WeatherReader(object):
         raise NotImplementedError
 
 class DailyWeatherReader(WeatherReader):
+    """Exposes daily weather data, split into yearly files."""
+
     def __init__(self, template, year1, variable):
         self.template = template
         self.year1 = year1
@@ -33,6 +35,7 @@ class DailyWeatherReader(WeatherReader):
     def get_times(self):
         years = []
 
+        # Look for available yearly files
         year = self.year1
         while os.path.exists(self.template % (year)):
             years.extend(year * 1000 + np.arange(1, 366))
@@ -44,12 +47,15 @@ class DailyWeatherReader(WeatherReader):
         return [self.variable]
 
     def read_iterator(self):
+        # Yield data in yearly chunks
         year = self.year1
         while os.path.exists(self.template % (year)):
             yield netcdfs.readncdf(self.template % (year), self.variable)
             year += 1
 
 class BinnedWeatherReader(WeatherReader):
+    """Exposes binned weather data, accumulated into months and split into yearly file."""
+
     def __init__(self, template, year1, variable):
         self.template = template
         self.year1 = year1
@@ -58,6 +64,7 @@ class BinnedWeatherReader(WeatherReader):
     def get_times(self):
         years = []
 
+        # Look for available yearly files
         year = self.year1
         while os.path.exists(self.template % (year)):
             years.extend(year * 1000 + np.arange(1, 13))
@@ -69,14 +76,16 @@ class BinnedWeatherReader(WeatherReader):
         return [self.variable]
 
     def read_iterator(self):
+        # Yield data in yearly chunks
         year = self.year1
         while os.path.exists(self.template % (year)):
             times, mmbbrr = netcdfs.readncdf_binned(self.template % (year), self.variable)
-            mmrrbb = np.swapaxes(mmbbrr, 1, 2)
+            mmrrbb = np.swapaxes(mmbbrr, 1, 2) # Needs to be in T x REGIONS x K
             yield times, mmrrbb
             year += 1
 
 if __name__ == '__main__':
+    # Check the first month of daily values
     template1 = "/shares/gcp/BCSD/grid2reg/cmip5/historical/CCSM4/tas/tas_day_aggregated_historical_r1i1p1_CCSM4_%d.nc"
     weatherreader1 = DailyWeatherReader(template1, 1981, 'tas')
 
@@ -87,6 +96,7 @@ if __name__ == '__main__':
         print weather[:31, 1000]
         break
 
+    # Compare it to the first month of binned values
     template2 = "/shares/gcp/BCSD/grid2reg/cmip5_bins/historical/CCSM4/tas/tas_Bindays_aggregated_historical_r1i1p1_CCSM4_%d.nc"
     weatherreader2 = BinnedWeatherReader(template2, 1981, 'DayNumber')
 
