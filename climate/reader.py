6fb852cc5ea91f269e7cf11a096843c6427476da
James Rising <jarising@gmail.com>
2016-08-18 14:08:41 -0700

    basic system for Jiacan

--- climate/reader.py
+++ climate/reader.py
@@ -18,7 +18,7 @@ class WeatherReader(object):
         """Yields a tuple (times, weathers) in whatever chunks are convenient.
 
         times should be a numpy array with a single dimension.  Let it have length T.
-        weather should be a numpy array of size REGIONS x T [x K]
+        weather should be a numpy array of size T x REGIONS [x K]
           the last dimension is optional, if more than one value is returned for each time.
         """
         raise NotImplementedError
@@ -44,16 +44,52 @@ class DailyWeatherReader(WeatherReader):
 
     def read_iterator(self):
         year = self.year1
-        while os.path.exists(self.template % (year)) and year <= maxyear:
+        while os.path.exists(self.template % (year)):
             yield netcdfs.readncdf(self.template % (year), self.variable)
             year += 1
 
+class BinnedWeatherReader(WeatherReader):
+    def __init__(self, template, year1, variable):
+        self.template = template
+        self.year1 = year1
+        self.variable = variable
+
+    def get_times(self):
+        years = []
+
+        year = self.year1
+        while os.path.exists(self.template % (year)):
+            years.extend(year * 1000 + np.arange(1, 13))
+            year += 1
+
+        return years
+
+    def get_dimension(self):
+        return [self.variable]
+
+    def read_iterator(self):
+        year = self.year1
+        while os.path.exists(self.template % (year)):
+            times, mmbbrr = netcdfs.readncdf_binned(self.template % (year), self.variable)
+            mmrrbb = np.swapaxes(mmbbrr, 1, 2)
+            yield times, mmrrbb
+            year += 1
+
 if __name__ == '__init__':
-    template = "/shares/gcp/BCSD/grid2reg/cmip5/historical/CCSM4/tas/tas_day_aggregated_historical_r1i1p1_CCSM4_%d.nc"
-    weatherreader = DailyWeatherReader(template, 2006, 'tas')
+    template1 = "/shares/gcp/BCSD/grid2reg/cmip5/historical/CCSM4/tas/tas_day_aggregated_historical_r1i1p1_CCSM4_%d.nc"
+    weatherreader1 = DailyWeatherReader(template1, 2006, 'tas')
 
-    print weatherreader.get_times()[:10]
+    print weatherreader1.get_times()[:31]
 
-    for times, weather in weatherreader.read_iterator():
-        print times[:10]
-        print weather[1000, :10]
+    for times, weather in weatherreader1.read_iterator():
+        print times[:31]
+        print weather[1000, :31]
+
+    template2 = "/shares/gcp/BCSD/grid2reg/cmip5/historical/CCSM4/tas/tas_day_aggregated_historical_r1i1p1_CCSM4_%d.nc"
+    weatherreader2 = BinnedWeatherReader(template2, 2006, 'tas')
+
+    print weatherreader2.get_times()[:2]
+
+    for times, weather in weatherreader2.read_iterator():
+        print times[:2]
+        print weather[1000, 0]
