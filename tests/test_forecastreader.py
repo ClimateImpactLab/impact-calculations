84bc9d461293b0e83fb2583b49accb6cb448e2ce
James Rising <jarising@gmail.com>
2016-08-25 08:23:47 -0700

    compare z-score to self-made

--- tests/test_forecastreader.py
+++ tests/test_forecastreader.py
@@ -5,16 +5,17 @@ from climate import forecasts
 
 class TestForecastReader(unittest.TestCase):
     def test_zscore(self):
-        weatherreader = MonthlyForecastReader(forecasts.temp_path, 'temp')
-        weatherreader_z = MonthlyZScoreForecastReader(forecasts.temp_path, forecasts.temp_climate_path, 'temp')
+        weatherreader_z = MonthlyForecastReader(forecasts.temp_zscores_path, 'z-scores')
+        z_weatherreader = MonthlyZScoreForecastReader(forecasts.temp_path, forecasts.temp_climate_path, 'temp')
 
-        w_iterator = weatherreader.read_iterator()
         wz_iterator = weatherreader_z.read_iterator()
+        wz_iterator = z_weatherreader.read_iterator()
 
         for month1, weather1 in w_iterator:
             month2, weather2 = wz_iterator.next()
 
             self.assertEqual(month1, month2)
+            np.assert_allclose(weather1, weather2)
             print weather1[1000], weather2[1000]
 
 if __name__ == '__main__':
