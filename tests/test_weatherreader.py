0f7fb870d99454ac6beac261b9098300cb8b9c64
James Rising <jarising@gmail.com>
2016-08-27 19:08:16 -0700

    check all regions

--- tests/test_weatherreader.py
+++ tests/test_weatherreader.py
@@ -6,7 +6,7 @@ from climate.dailyreader import *
 class TestWeatherReader(unittest.TestCase):
     def test_bincompare(self):
         # Check the first month of daily values
-        print "Reading from the daily weather data files."
+        print "Reading from the daily data files."
         template1 = "/shares/gcp/BCSD/grid2reg/cmip5/historical/CCSM4/tas/tas_day_aggregated_historical_r1i1p1_CCSM4_%d.nc"
         weatherreader1 = DailyWeatherReader(template1, 1981, 'tas')
 
@@ -15,11 +15,7 @@ class TestWeatherReader(unittest.TestCase):
 
         self.assertEqual(len(weatherreader1.get_dimension()), 1)
 
-        times_january = weatherreader1.get_times()[:31]
-        for times, weather in weatherreader1.read_iterator():
-            testing.assert_array_equal(times_january, times[:31])
-            daily_january = weather[:31, 1001]
-            break
+        alltimes1 = weatherreader1.get_times()
 
         # Compare it to the first month of binned values
         print "Reading from the binned data files."
@@ -31,20 +27,33 @@ class TestWeatherReader(unittest.TestCase):
 
         self.assertEqual(len(weatherreader2.get_dimension()), 12)
 
-        times_janfeb = weatherreader2.get_times()[:2]
-        for times, weather in weatherreader2.read_iterator():
-            testing.assert_array_equal(times_janfeb, times[:2])
-            binned_january = weather[0, 1001]
-            print binned_january
-            print daily_january
+        lltimes2 = weatherreader2.get_times()
 
-            # Check that bins match expected
+        iterator1 = weatherreader1.read_iterator()
+        iterator2 = weatherreader2.read_iterator()
+        while alltimes1 or alltimes2:
+            times1, weather1 = iterator1.next()
+            times2, weather2 = iterator2.next()
+
+            testing.assert_array_equal(alltimes1[:len(times1)], times1)
+            alltimes1 = alltimes1[len(times1):]
+
+            testing.assert_array_equal(alltimes2[:len(times2)], times2)
+            alltimes2 = alltimes2[len(times2):]
+
             binlimits = [-np.inf, -17, -12, -7, -2, 3, 8, 13, 18, 23, 28, 33, np.inf]
-            for ii in range(0, len(binlimits)-1):
-                days = np.sum((daily_january >= binlimits[ii]) & (daily_january < binlimits[ii+1]))
-                print binlimits[ii], binlimits[ii+1], days
-                self.assertEqual(days, binned_january[ii])
-            break
+            dayspermonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+            monthlimits = [0] + list(np.cumsum(dayspermonth))
+
+            self.assertEqual(weather1.shape[1], weather2.shape[2])
+
+            for region in range(weather1.shape[1]): # iterate through regions
+                for month in range(12):
+                    monthtemps = weather1[monthlimits[month]:monthlimits[month+1], region]
+                    for ii in range(len(binlimits)-1):
+                        days = np.sum((monthtemps >= binlimits[ii]) & (monthtemps < binlimits[ii+1]))
+                        # Check that bins match expected
+                        self.assertEqual(days, weather2[month, region, ii])
 
 if __name__ == '__main__':
     unittest.main()
