2b4c6645ac471f92047f7eef6bc1f3d31ed58d94
James Rising <jarising@gmail.com>
2016-09-09 09:09:49 -0700

    aggregation system

---
+++ generate/agglib.py
@@ -0,0 +1,56 @@
+import csv
+from helpers import header
+
+def copy_timereg_variable(writer, variable, key, dstvalues, suffix, timevar='year'):
+    column = writer.createVariable(key, 'f8', (timevar, 'region'))
+    column.units = variable.units
+    if hasattr(variable, 'long_title'):
+        column.long_title = variable.long_title
+    if hasattr(variable, 'source'):
+        column.source = variable.source + " " + suffix
+
+    column[:, :] = dstvalues
+
+def iter_timereg_variables(reader, timevar='year'):
+    for key in reader.variables.keys():
+        if (timevar, 'region') == reader.variables[key].dimensions:
+            print key
+            variable = reader.variables[key]
+
+            yield key, variable
+
+def get_aggregated_regions(regions):
+    # Collect all levels of aggregation
+    originals = {} # { prefix: [region] }
+    for region in regions:
+        original = region
+        while len(region) > 3:
+            region = region[:region.rindex('.')]
+            if region in originals:
+                originals[region].append(original)
+            else:
+                originals[region] = [original]
+
+    # Add the FUND regions
+    dependencies = []
+    with open('../socioeconomics/aggloms/world/macro-regions.csv', 'r') as fp:
+        aggreader = csv.reader(header.deparse(fp, dependencies))
+        headrow = aggreader.next()
+        for row in aggreader:
+            fundregion = 'FUND-' + row[headrow.index('FUND')]
+            if fundregion not in originals:
+                originals[fundregion] = []
+
+            iso3 = row[headrow.index('region-key')]
+            if iso3 not in originals:
+                continue
+
+            originals[fundregion].extend(originals[iso3])
+
+    # Collect all prefixes with > 1 region
+    prefixes = [''] # '' = world
+    for prefix in originals:
+        if originals[prefix] > 1:
+            prefixes.append(prefix)
+
+    return originals, prefixes, dependencies
---
+++ shortterm/aggregate.py
@@ -0,0 +1,96 @@
+import os, Queue
+import numpy as np
+from netCDF4 import Dataset
+from impacts import nc4writer
+from generate import agglib
+
+suffix = "-aggregated"
+checkfile = 'check-2016-05-02.txt'
+
+def iterdir(basedir):
+    # Do median first
+    if 'median' in os.listdir(basedir):
+        yield 'median', os.path.join(os.path.join(basedir, 'median'))
+    for filename in os.listdir(basedir):
+        if filename != 'median':
+            yield filename, os.path.join(os.path.join(basedir, filename))
+
+def iterresults(outdir):
+    for batch1, batch1path in iterdir(outdir):
+        for batch2, batch12path in iterdir(batch1path):
+            yield batch12path
+
+def make_aggregates(targetdir, filename, stweight):
+    # Find all variables that containing the region dimension
+    reader = Dataset(os.path.join(targetdir, filename), 'r', format='NETCDF4')
+    readermonths = reader.variables['month'][:]
+
+    writer = Dataset(os.path.join(targetdir, filename[:-4] + suffix + '.nc4'), 'w', format='NETCDF4')
+
+    regions = reader.variables['regions'][:].tolist()
+    originals, prefixes, dependencies = agglib.get_aggregated_regions(regions)
+
+    writer.description = reader.description + " (aggregated)"
+    writer.version = reader.version
+    writer.dependencies = ', '.join(dependencies) + ', ' + reader.version
+    writer.author = reader.author
+
+    months = nc4writer.make_months_variable(writer, 'Months since 1960-01-01')
+    months[:] = readermonths
+
+    month2years1981 = []
+    for month in months:
+        month2years1981 = int(month / 12) - 21 # month values to year values, with 0 = 1981
+
+    nc4writer.make_regions_variable(writer, prefixes, 'aggregated')
+
+    original_indices = {regions[ii]: ii for ii in range(len(regions))}
+
+    for key, variable in agglib.iter_timereg_variables(reader, timevar='month'):
+        dstvalues = np.zeros((len(months), len(prefixes)))
+        srcvalues = variable[:, :]
+        for ii in range(len(prefixes)):
+            numers = np.zeros(srcvalues.shape[0])
+            denoms = np.zeros(srcvalues.shape[0])
+
+            if prefixes[ii] == '':
+                withinregions = regions
+            else:
+                withinregions = originals[prefixes[ii]]
+
+            for original in withinregions:
+                weights = stweight.get_time(original)[month2years1981]
+                numers += weights * srcvalues[:, original_indices[original]]
+                denoms += weights
+
+            dstvalues[:, ii] = numers / denoms
+
+        agglib.copy_timereg_variable(writer, variable, key, dstvalues, "(aggregated)", timevar='month')
+
+    reader.close()
+    writer.close()
+
+if __name__ == '__main__':
+    import sys
+    from datastore import population
+    outputdir = sys.argv[1]
+
+    halfweight = population.SpaceTimeBipartiteData(1981, 2020, None)
+    stweight = halfweight.load_population(1981, 2020, 'OECD Env-Growth', 'SSP2_v9_130325')
+
+    for targetdir in iterresults(outputdir):
+        print targetdir
+
+        with open(os.path.join(targetdir, checkfile), 'w') as fp:
+            fp.write("START")
+
+        for filename in os.listdir(targetdir):
+            if filename[-4:] == '.nc4' and suffix not in filename:
+                print filename
+
+                # Aggregate impacts
+                make_aggregates(targetdir, filename, stweight)
+
+        with open(os.path.join(targetdir, checkfile), 'w') as fp:
+            fp.write("END")
+
