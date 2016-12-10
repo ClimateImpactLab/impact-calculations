4c6efc6f244ca0416bc820073a79fff70e44b199
James Rising <jarising@gmail.com>
2016-12-10 15:24:52 -0800

    set up for run

--- generate/medrun.sh
+++ generate/medrun.sh
@@ -1,11 +1,3 @@
 source ~/aggregator/env/bin/activate
 cd ..
-#python -m generate.median /shares/gcp/outputs/nasmort
-
-for i in {1..5}
-do
-    nohup python -m generate.allmodels median mortality /shares/gcp/outputs/tmp >& median$i.log &
-    sleep 5
-done
-
-
+python -m generate.allmodels median mortality /shares/gcp/outputs/mortality/impacts-pharaoh
