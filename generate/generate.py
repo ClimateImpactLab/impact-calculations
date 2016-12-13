724a3765b5a4c29a06e7e13c9eceb2f87b2fe7fb
James Rising <jarising@gmail.com>
2016-12-13 11:12:31 -0800

    add in writebin behavior

--- generate/generate.py
+++ generate/generate.py
@@ -26,7 +26,32 @@ def iterate_single():
 
     yield 'single', pvals, clim_scenario, clim_model, weatherbundle, econ_scenario, econ_model, economicmodel
 
-mode_iterators = {'median': iterate_median, 'montecarlo': iterate_montecarlo, 'single': iterate_single}
+def iterate_writebin():
+    with open("allbins.csv", 'w') as fp:
+        writer = csv.writer(fp)
+        writer.writerow(['region', 'year', 'model', 'result', 'bin_nInfC_n17C', 'bin_n17C_n12C', 'bin_n12C_n7C', 'bin_n7C_n2C', 'bin_n2C_3C', 'bin_3C_8C', 'bin_8C_13C', 'bin_13C_18C', 'bin_18C_23C', 'bin_23C_28C', 'bin_28C_33C', 'bin_33C_InfC'])
+
+    with open("allpreds.csv", 'w') as fp:
+        writer = csv.writer(fp)
+        writer.writerow(['region', 'year', 'meandays_nInfC_n17C', 'meandays_n17C_n12C', 'meandays_n12C_n7C', 'meandays_n7C_n2C', 'meandays_n2C_3C', 'meandays_3C_8C', 'meandays_8C_13C', 'meandays_13C_18C', 'meandays_23C_28C', 'meandays_28C_33C', 'meandays_33C_InfC', 'log gdppc', 'log popop'])
+
+    for allvals in iterate_single():
+        yield allvals
+
+def result_callback(region, year, result, calculation, model):
+    with open("allbins.csv", 'a') as fp:
+        writer = csv.writer(fp)
+        curve = adapting_curve.region_stepcurves[region].curr_curve
+        writer.writerow([region, year, model, result[0]] + list(curve.yy))
+
+def push_callback(region, year, application, get_predictors):
+    with open("allpreds.csv", 'a') as fp:
+        writer = csv.writer(fp)
+        predictors = get_predictors(region)
+        writer.writerow([region, year] + list(predictors[0]))
+
+
+mode_iterators = {'median': iterate_median, 'montecarlo': iterate_montecarlo, 'single': iterate_single, 'writebin': iterate_writebin}
 
 mode = sys.argv[1]
 assert mode in mode_iterators.keys()
@@ -52,8 +77,11 @@ for batchdir, pvals, clim_scenario, clim_model, weatherbundle, econ_scenario, ec
     os.makedirs(targetdir)
 
     effectset.make_pval_file(targetdir, pvals)
-    mod.produce(targetdir, weatherbundle, economicmodel, get_model, pvals, do_only=do_only, do_farmers=True)
-
+    if mode == 'writebins':
+        mod.produce(targetdir, weatherbundle, economicmodel, get_model, pvals, do_only=do_only, do_farmers=False, result_callback=result_callback, push_callback=push_callback)
+    else:
+        mod.produce(targetdir, weatherbundle, economicmodel, get_model, pvals, do_only=do_only, do_farmers=True)
+        
     # Generate historical baseline
     historybundle = weather.RepeatedHistoricalWeatherBundle.make_historical(weatherbundle, None if mode == 'median' else pvals['histclim'].get_seed())
     pvals.lock()
