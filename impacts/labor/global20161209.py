91a58e508ea03d767764d852821e713fcb5aa7df
James Rising <jarising@gmail.com>
2016-12-13 19:05:58 -0800

    fix how reordered

--- impacts/labor/global20161209.py
+++ impacts/labor/global20161209.py
@@ -9,7 +9,17 @@ from adaptation import csvvfile
 def prepare_csvv(csvvpath, qvals, callback):
     data = csvvfile.read(csvvpath)
     data = csvvfile.collapse_bang(qvals.get_seed())
-    
+
+    assert data['prednames'] == ['tempL0', 'temp2L0', 'temp3L0', 'temp4L0', 'tempL0lninc', 'temp2L0lninc', 'temp3L0lninc', 'temp4L0lninc', 'tempL0lnpop', 'temp2L0lnpop', 'temp3L0lnpop', 'temp4L0lnpop', 'tempL0LRTMAX', 'temp2L0LRTMAX', 'temp3L0LRTMAX', 'temp4L0LRTMAX', 'belowzero']
+    # Switch to be const, tasmax, loggdppc, logpopop
+    oldindices = np.array(0, 3, 1, 2)
+    newindices = np.arange(4)
+    for kk in range(4):
+        csvv['prednames'][kk + newindices * 4] = csvv['prednames'][kk + oldindices * 4]
+        csvv['gamma'][kk + newindices * 4] = csvv['gamma'][kk + oldindices * 4]
+        csvv['gammavcv'][kk + newindices * 4, :] = csvv['gammavcv'][kk + oldindices * 4, :]
+        csvv['gammavcv'][:, kk + newindices * 4] = csvv['gammavcv'][:, kk + oldindices * 4]
+
     polyvals = csvvfile.extract_values(data, range(4), r"temp{K}?L0.*")
     tempcurve = curvegen.PolynomialCurveGenerator(4, 'C', 'minutes', None, polyvals['gamma'], polyvals['gammavcv'], polyvals['residvcv'], callback=lambda r, x, y: callback('temp', r, x, y))
 
