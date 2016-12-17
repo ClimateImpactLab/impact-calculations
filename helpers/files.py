056dfcd47654b48af235bd5f628bae2878085cfe
James Rising <jarising@gmail.com>
2016-12-16 21:43:42 -0800

    add default configs

--- helpers/files.py
+++ helpers/files.py
@@ -1,7 +1,7 @@
 import os
 
 def sharedpath(subpath):
-    return os.path.join(server_config['shareddir'], subpath)
+    return os.path.join(serverConfig['shareddir'], subpath)
 
 if __name__ == '__main__':
     print datapath('testing')
