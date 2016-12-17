5cee730a2939977d7a469a334b32725a84e535f5
James Rising <jarising@gmail.com>
2016-12-16 21:39:09 -0800

    add config system

---
+++ helpers/config.py
@@ -0,0 +1,23 @@
+import sys
+import yaml
+
+def getConfigDictFromFile(filePath):
+    with open(filePath, 'r') as f:
+        confDict = yaml.load(f)
+    return confDict
+
+def getConfigDictFromSysArgv():
+    with open(sys.argv[1], 'r') as f:
+        confDict = yaml.load(f)
+
+        for key in serverConfig:
+            if key not in confDict:
+                confDict[key] = serverConfig[key]
+
+        return confDict
+
+## Server config
+
+serverSpecificYML = "../server_config.yml"
+
+serverConfig = getConfigDictFromFile(serverSpecificYML)
