a237d8ddc05c35042bce2cf53a224f244662398f
James Rising <jarising@gmail.com>
2016-12-17 16:43:01 -0800

    was collapsing with MC even under median

--- impacts/labor/allmodels.py
+++ impacts/labor/allmodels.py
@@ -31,7 +31,7 @@ def produce(targetdir, weatherbundle, economicmodel, get_model, pvals, do_only=N
                 effectset.small_test(weatherbundle, calculation, None, num_regions=10)
                 return
             else:
-                effectset.write_ncdf(targetdir, basename, weatherbundle, calculation, None, "Extensive margin labor impacts, with interpolation and adaptation through interpolation.", dependencies + weatherbundle.dependencies + economicmodel.dependencies, result_callback=lambda reg, yr, res, calc: result_callback(reg, yr, res, calc, basename), push_callback=lambda reg, yr, app: push_callback(reg, yr / 1000, app, baseline_get_predictors, basename), do_interpbins=False, suffix=suffix)
+                effectset.write_ncdf(targetdir, basename, weatherbundle, calculation, None, "Extensive margin labor impacts, with interpolation and adaptation through interpolation.", dependencies + weatherbundle.dependencies + economicmodel.dependencies, result_callback=lambda reg, yr, res, calc: result_callback(reg, yr, res, calc, basename), push_callback=lambda reg, yr, app: push_callback(reg, int(yr) / 1000, app, baseline_get_predictors, basename), do_interpbins=False, suffix=suffix)
 
             if do_farmers and not weatherbundle.is_historical():
                 # Lock in the values
@@ -40,9 +40,9 @@ def produce(targetdir, weatherbundle, economicmodel, get_model, pvals, do_only=N
                 # Comatose Farmer
                 print "Comatose Farmer"
                 calculation, dependencies, baseline_get_predictors = caller.call_prepare_interp2(filepath, 'impacts.labor.global20161209_comatose', weatherbundle, economicmodel, pvals[basename], callback=lambda v, r, x, y: None)
-                effectset.write_ncdf(targetdir, basename + "Comatose", weatherbundle, calculation, None, "Extensive margin labor impacts, with no adaptation.", dependencies + weatherbundle.dependencies + economicmodel.dependencies, result_callback=lambda reg, yr, res, calc: result_callback(reg, yr, res, calc, basename + '-coma'), push_callback=lambda reg, yr, app: push_callback(reg, yr / 1000, app, baseline_get_predictors, basename), do_interpbins=False, suffix=suffix)
+                effectset.write_ncdf(targetdir, basename + "Comatose", weatherbundle, calculation, None, "Extensive margin labor impacts, with no adaptation.", dependencies + weatherbundle.dependencies + economicmodel.dependencies, result_callback=lambda reg, yr, res, calc: result_callback(reg, yr, res, calc, basename + '-coma'), push_callback=lambda reg, yr, app: push_callback(reg, int(yr) / 1000, app, baseline_get_predictors, basename), do_interpbins=False, suffix=suffix)
 
                 # Dumb Farmer
                 print "Dumb Farmer"
                 calculation, dependencies, baseline_get_predictors = caller.call_prepare_interp2(filepath, 'impacts.labor.global20161209_dumb', weatherbundle, economicmodel, pvals[basename], callback=lambda v, r, x, y: None)
-                effectset.write_ncdf(targetdir, basename + "Dumb", weatherbundle, calculation, None, "Extensive margin labor impacts, with interpolation and only environmental adaptation.", dependencies + weatherbundle.dependencies + economicmodel.dependencies, result_callback=lambda reg, yr, res, calc: result_callback(reg, yr, res, calc, basename + '-dumb'), push_callback=lambda reg, yr, app: push_callback(reg, yr / 1000, app, baseline_get_predictors, basename), do_interpbins=False, suffix=suffix)
+                effectset.write_ncdf(targetdir, basename + "Dumb", weatherbundle, calculation, None, "Extensive margin labor impacts, with interpolation and only environmental adaptation.", dependencies + weatherbundle.dependencies + economicmodel.dependencies, result_callback=lambda reg, yr, res, calc: result_callback(reg, yr, res, calc, basename + '-dumb'), push_callback=lambda reg, yr, app: push_callback(reg, int(yr) / 1000, app, baseline_get_predictors, basename), do_interpbins=False, suffix=suffix)
