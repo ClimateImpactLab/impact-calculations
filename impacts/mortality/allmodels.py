ede9e1a2971f5d8b98cb4d985d5949662fe38fcd
James Rising <jarising@gmail.com>
2016-12-21 14:06:41 -0800

    fixes to mortality

--- impacts/mortality/allmodels.py
+++ impacts/mortality/allmodels.py
@@ -10,7 +10,7 @@ def preload():
 
 bundle_iterator = weather.iterate_bundles(discover_tas_binned('/shares/gcp/climate/BCSD/aggregation/cmip5_bins/IR_level'))
 
-def produce(targetdir, weatherbundle, economicmodel, get_model, pvals, do_only=None, country_specific=True, result_callback=None, push_callback=None, suffix='', do_farmers=False, do_65plus=True, profile=False):
+def produce(targetdir, weatherbundle, economicmodel, get_model, pvals, do_only=None, country_specific=True, result_callback=None, push_callback=None, suffix='', do_farmers=False, do_65plus=True, profile=False, redocheck=None, diagnosefile=False):
     print do_only
     if do_only is None or do_only == 'acp':
         # ACP response
@@ -40,7 +40,8 @@ def produce(targetdir, weatherbundle, economicmodel, get_model, pvals, do_only=N
                 effectset.small_test(weatherbundle, calculation, baseline_get_predictors, num_regions=10)
                 return
             else:
-                effectset.write_ncdf(targetdir, basename, weatherbundle, calculation, baseline_get_predictors, "Mortality impacts, with interpolation and adaptation through interpolation.", dependencies + weatherbundle.dependencies + economicmodel.dependencies, result_callback=lambda reg, yr, res, calc: result_callback(reg, yr, res, calc, basename), push_callback=lambda reg, yr, app: push_callback(reg, yr, app, baseline_get_predictors, basename), do_interpbins=do_interpbins, suffix=suffix)
+                effectset.write_ncdf(targetdir, basename, weatherbundle, calculation, baseline_get_predictors, "Mortality impacts, with interpolation and adaptation through interpolation.", dependencies + weatherbundle.dependencies + economicmodel.dependencies, result_callback=lambda reg, yr, res, calc: result_callback(reg, yr, res, calc, basename), push_callback=lambda reg, yr, app: push_callback(reg, yr, app, baseline_get_predictors, basename), do_interpbins=do_interpbins, suffix=suffix, diagnosefile=diagnosefile)
+                diagnosefile = False # Only do first
 
             if do_farmers and not weatherbundle.is_historical():
                 # Lock in the values
