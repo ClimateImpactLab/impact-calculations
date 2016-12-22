36fa33c13a0fbb9b8ce2a24af1c72f8d7cbd8c76
James Rising <jarising@gmail.com>
2016-12-22 15:02:44 -0800

    partial out

--- generate/checks.py
+++ generate/checks.py
@@ -8,7 +8,7 @@ def check_result_100years(filepath, variable='rebased'):
     if values.shape[0] < 100 or values.shape[1] < 20000:
         return False
 
-    if np.isnan(values[100, 10000]):
+    if np.isnan(values[100, 10000]) or np.all(values[100, :] == 0):
         return False
 
     return True
