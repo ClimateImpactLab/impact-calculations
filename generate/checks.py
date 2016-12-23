80e0fe6914078f65822c942662946e46683e8e3b
James Rising <jarising@gmail.com>
2016-12-23 09:42:47 -0800

    updates

--- generate/checks.py
+++ generate/checks.py
@@ -2,13 +2,16 @@ from netCDF4 import Dataset
 import numpy as np
 
 def check_result_100years(filepath, variable='rebased'):
-    rootgrp = Dataset(filepath, 'r', format='NETCDF4')
-    values = rootgrp.variables[variable][:, :]
+    try:
+        rootgrp = Dataset(filepath, 'r', format='NETCDF4')
+        values = rootgrp.variables[variable][:, :]
 
-    if values.shape[0] < 100 or values.shape[1] < 20000:
+        if values.shape[0] < 100 or values.shape[1] < 20000:
+            return False
+
+        if np.isnan(values[100, 10000]) or np.all(values[100, :] == 0):
+            return False
+
+        return True
+    except:
         return False
-
-    if np.isnan(values[100, 10000]) or np.all(values[100, :] == 0):
-        return False
-
-    return True
