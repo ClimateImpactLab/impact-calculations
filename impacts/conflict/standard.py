49008026b3e470b17a2738176fdb091479e9f493
James Rising <jarising@gmail.com>
2016-12-24 00:08:57 -0800

    better checking, and conflict

--- impacts/conflict/standard.py
+++ impacts/conflict/standard.py
@@ -9,6 +9,7 @@ from openest.generate.stdlib import *
 from adaptation import csvvfile
 from shortterm import curvegen, weather
 from climate import forecasts, forecastreader
+from openest.models.curve import PolynomialCurve
 
 def prepare_csvv(csvvpath, qvals, callback):
     data = csvvfile.read(csvvpath)
@@ -18,13 +19,11 @@ def prepare_csvv(csvvpath, qvals, callback):
     tggr = data['gamma'][0:4]
     pggr = data['gamma'][-2:]
 
-    tcurve = curvegen.LinearCurveGenerator('C', 'rate', qvals.get_seed(), tggr, None, tggr['residvcv'], callback=lambda r, x, y: callback('temp', r, x, y))
-    qvals.lock() # Use consistent seeds at this point
+    tcurve = curvegen.LinearCurveGenerator('C', 'rate', None, tggr, None, data['residvcv'], callback=lambda r, x, y: callback('temp', r, x, y))
 
     teffect = SingleWeatherApply('rate', tcurve, 'the linear temperature effect', lambda tp: tp[0])
 
-    pggr = csvvfile.extract_values(data, range(1, 4))
-    p3curve = curvegen.PolynomialCurveGenerator(3, 'mm/month', 'rate', qvals.get_seed(1), pggr, None, pggr['residvcv'], callback=lambda r, x, y: callback('prcp', r, x, y))
-    p3effect = SingleWeatherApply('rate', p3curve, 'the quadratic precipitation effect', lambda tp: (365.25 / 30) * (tp[1]**2))
+    p2curve = PolynomialCurve([-np.inf, np.inf], pggr)
+    p2effect = SingleWeatherApply('rate', p2curve, 'the quadratic precipitation effect', lambda tp: (365.25 / 30) * (tp[1]**2))
 
-    return Sum([teffect, p3effect]), [data['attrs']['version']], data['prednames']
+    return Sum([teffect, p2effect]), [data['attrs']['version']], data['prednames']
