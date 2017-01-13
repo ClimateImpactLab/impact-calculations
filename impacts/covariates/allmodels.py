c492b8e5d368af4aa73cbcfc853f39ecd4d471ec
James Rising <jarising@gmail.com>
2017-01-13 06:44:53 -0800

    produce histclim results

--- impacts/covariates/allmodels.py
+++ impacts/covariates/allmodels.py
@@ -13,9 +13,6 @@ def get_bundle_iterator():
     return weather.iterate_bundles(discover_variable(files.sharedpath('climate/BCSD/aggregation/cmip5/IR_level'), 'tas'))
 
 def produce(targetdir, weatherbundle, economicmodel, get_model, pvals, do_only=None, country_specific=True, result_callback=None, push_callback=None, suffix='', do_farmers=False, profile=False, redocheck=False, diagnosefile=False):
-    if suffix != '':
-        return
-
     predgen = covariates.CombinedCovariator([covariates.MeanWeatherCovariator(weatherbundle, 15, 2015),
                                              covariates.EconomicCovariator(economicmodel, 3, 2015)])
     covars = ['tas', 'loggdppc', 'logpopop']
@@ -25,4 +22,4 @@ def produce(targetdir, weatherbundle, economicmodel, get_model, pvals, do_only=N
                              ['Mean temperature', 'log GDP per capita', 'Log population-weighted population density'],
                              ['Last 15 years of temperature', 'Estimate GDP per capita', 'Population density, as weighted by population'])
 
-    effectset.write_ncdf(targetdir, 'Covariates', weatherbundle, calculation, None, "Extensive margin labor impacts, with interpolation and adaptation through interpolation.", weatherbundle.dependencies + economicmodel.dependencies)
+    effectset.write_ncdf(targetdir, 'Covariates', weatherbundle, calculation, None, "Extensive margin labor impacts, with interpolation and adaptation through interpolation.", weatherbundle.dependencies + economicmodel.dependencies, suffix=suffix)
