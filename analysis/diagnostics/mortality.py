15b4eba4fcf8b38a5ebafee16c0cecdaef6894e8
James Rising <jarising@gmail.com>
2017-03-18 23:33:56 -0700

    updates

--- analysis/diagnostics/mortality.py
+++ analysis/diagnostics/mortality.py
@@ -27,10 +27,15 @@ for year in [2009, 2050]:
     lib.show_header("Calc. of tas_sum coefficient in %d (%f reported)" % (year, lib.excind(coeffs, year, 'tas_sum')))
     lib.show_coefficient(csvv, preds, year, 'spline_variables-0', {})
 
-lib.show_header("Calc. of baseline (%f reported)" % (lib.excind(calcs, 2009, 'baseline')))
 coefflist = ['tas_sum'] + ['spline_variables-%d' % ii for ii in range(1, 7)]
+
+lib.show_header("Calc. of minimum point temperature (%f reported)", % lib.excind(calcs, 2009, 'mintemp'))
+curve = CubicSplineCurve([-12, -7, 0, 10, 18, 23, 28, 33], [lib.excind(coeffs, 2009, coeff) for coeff in coefflist])
+print curve(np.arange(10, 26))
+
+lib.show_header("Calc. of baseline (%f reported)" % (lib.excind(calcs, 2009, 'baseline')))
 lines = ["bl1(weather) = sum([%s]' * weather) / 100000" % ', '.join(["%.12g" % lib.excind(coeffs, 2009, coeff) for coeff in coefflist]),
-         "bl(weather) = bl1(weather) - bl1(365 * [20, 32768, 19683, 8000, 1000, 8, 0])"]
+         "bl(weather) = bl1(weather) - bl1(365 * [%s])" % (', '.join(["%d" % lib.excind(curve.get_terms(lib.excind(calcs, 2009, 'mintemp')))]))]
 terms = []
 for year in range(2001, 2011):
     terms.append("bl([%s])" % ', '.join(["%.12g" % lib.excind(calcs, year, 'var-%d' % ii) for ii in range(7)]))
@@ -39,6 +44,6 @@ lib.show_julia(lines)
 
 lib.show_header("Calc. of result (%f reported)" % (coeffs['2050'][0]))
 lines = ["ef1(weather) = sum([%s]' * weather) / 100000" % ', '.join(["%.12g" % lib.excind(coeffs, 2050, coeff) for coeff in coefflist]),
-         "ef(weather) = ef1(weather) - ef1(365 * [20, 32768, 19683, 8000, 1000, 8, 0])"]
+         "ef(weather) = ef1(weather) - ef1(365 * [%s])" % (', '.join(["%d" % lib.excind(curve.get_terms(lib.excind(calcs, 2009, 'mintemp')))]))]
 lines.append("ef([%s]) - %f" % (', '.join(["%.12g" % lib.excind(calcs, 2050, 'var-%d' % ii) for ii in range(7)]), lib.excind(calcs, 2050, 'baseline')))
 lib.show_julia(lines)
