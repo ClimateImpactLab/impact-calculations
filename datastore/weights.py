9ac77e782ef614cd2248ee255bcf1f30ebf8f207
James Rising <jarising@gmail.com>
2017-07-28 11:42:32 -0400

    add interpreters

---
+++ datastore/weights.py
@@ -0,0 +1,16 @@
+import population, agecohorts
+
+def interpret(config):
+    assert 'weighting' in config
+    econ_model = config['iam']
+    econ_scenario = config['ssp']
+
+    if config['weighting'] == 'population':
+        halfweight = population.SpaceTimeBipartiteData(1981, 2100, None)
+        return lambda year0, year1: halfweight.load_population(year0, year1, econ_model, econ_scenario)
+
+    if config['weighting'] in agecohorts.columns:
+        halfweight = agecohorts.SpaceTimeBipartiteData(1981, 2100, None)
+        return lambda year0, year1: halfweight.load_population(year0, year1, econ_model, econ_scenario, config['weights'])
+
+    raise ValueError("Unknown weighting.")
