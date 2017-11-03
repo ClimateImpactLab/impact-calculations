7792f39e3b983193a5a47891e6921877852154dc
James Rising <jarising@gmail.com>
2017-11-03 11:48:09 -0700

    update slurm scripts

--- computer/savio_single.sh
+++ computer/savio_single.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Job name:
-#SBATCH --job-name=mortality-montecarlo
+#SBATCH --job-name=impact-calculations
 # Partition:
 #SBATCH --partition=savio2_bigmem
 # Account:
@@ -8,24 +8,22 @@
 # QoS:
 #SBATCH --qos=laika_bigmem2_normal
 # Wall clock limit:
-#SBATCH --time=36:00:00
+#SBATCH --time=52:00:00
 
 ## Command(s) to run:
 module load python/2.7.8
 module load virtualenv
 
-source ../env/bin/activate
+source ../../env/bin/activate
 
 module load numpy
 
-for i in {1..4}
+for i in {1..23}
 do
-  python -m generate.generate configs/mortality-montecarlo.yml &
+  python -m generate.generate configs/energy-median.yml &
   sleep 5
-  # python -m generate.generate configs/labor-montecarlo.yml &
-  # sleep 5
 done
 
-python -m generate.generate configs/mortality-montecarlo.yml
+python -m generate.generate configs/energy-median.yml
 
-python -m generate.aggregate configs/mortality-aggregate.yml
+#python -m generate.aggregate configs/mortality-aggregate.yml
