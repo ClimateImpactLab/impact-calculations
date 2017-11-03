5f07e434f0c1e0e90df873c4679308651679618a
James Rising <jarising@gmail.com>
2017-11-03 14:46:05 -0500

    mege single improvements

--- datastore/weights.py
+++ datastore/weights.py
@@ -41,18 +41,18 @@ def interpret(config):
         regions = df['regions'].unique()
         return lambda year0, year1: spacetime.SpaceTimeLazyData(year0, year1, regions, lambda region: df[df.region == region])
 
+    return interpret_halfweight(config['weighting'])
+
+def get_weight_args(config):
     econ_model = config['iam']
     econ_scenario = config['ssp']
 
-    if config['weighting'] == 'population':
-        halfweight = population.SpaceTimeBipartiteData(1981, 2100, None)
-        return lambda year0, year1: halfweight.load_population(year0, year1, econ_model, econ_scenario)
-
+    args = [econ_model, econ_scenario]
+    
     if config['weighting'] in agecohorts.columns:
-        halfweight = agecohorts.SpaceTimeBipartiteData(1981, 2100, None)
-        return lambda year0, year1: halfweight.load_population(year0, year1, econ_model, econ_scenario, config['weighting'])
+        args.append(config['weighting'])
 
-    raise ValueError("Unknown weighting.")
+    return tuple(args)
 
 if __name__ == '__main__':
     import sys, csv
