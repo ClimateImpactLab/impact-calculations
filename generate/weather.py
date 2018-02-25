e57094abb860d90416b377168d824c0c6c3d209e
James Rising <jarising@gmail.com>
2018-02-25 02:45:25 -0800

    fix typo

--- generate/weather.py
+++ generate/weather.py
@@ -306,12 +306,12 @@ class HistoricalWeatherBundle(DailyWeatherBundle):
                 allds = fast_dataset.merge((allds, ds)) #xr.merge((allds, ds))
 
             # Correct the time - should generalize
-            if ds2['time'][0] < 10000:
-                ds2['time'] += year - pastyear # YYYY
-            elif ds2['time'][0] < 1000000:
-                ds2['time'] += (year - pastyear) * 100 # YYYYMM
+            if allds['time'][0] < 10000:
+                allds['time'] += year - pastyear # YYYY
+            elif allds['time'][0] < 1000000:
+                allds['time'] += (year - pastyear) * 100 # YYYYMM
             else:
-                ds2['time'] += (year - pastyear) * 1000 # YYYYDDD
+                allds['time'] += (year - pastyear) * 1000 # YYYYDDD
                 
             for year2, ds2 in self.transformer.push(year, allds):
                 yield year2, ds2
