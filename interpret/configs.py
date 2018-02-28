af6217f9412c8f239a55d36fd90dbaebc2f20a28
James Rising <jarising@gmail.com>
2018-02-28 07:53:25 -0800

    handle only-models in discover

--- interpret/configs.py
+++ interpret/configs.py
@@ -1,4 +1,15 @@
-import copy
+import yaml, copy
+
+def standardize(config):
+    for key in config:
+        asdash = key.replace('_', '-')
+        asscore = key.replace('-', '_')
+        if '-' in key and asscore not in config:
+            config[asscore] = config[key]
+        if '_' in key and asdash not in config:
+            config[asdash] = config[key]
+                
+    return config
 
 def merge(parent, child):
     result = copy.copy(parent)
