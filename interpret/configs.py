93d0f0d3c6e8337bcdaa5c219223d5c3bf033b14
James Rising <jarising@gmail.com>
2018-02-28 08:01:33 -0800

    need to split out changes

--- interpret/configs.py
+++ interpret/configs.py
@@ -1,15 +1,17 @@
 import yaml, copy
 
 def standardize(config):
+    newconfig = copy.copy(config)
+
     for key in config:
         asdash = key.replace('_', '-')
         asscore = key.replace('-', '_')
         if '-' in key and asscore not in config:
-            config[asscore] = config[key]
+            newconfig[asscore] = config[key]
         if '_' in key and asdash not in config:
-            config[asdash] = config[key]
+            newconfig[asdash] = config[key]
                 
-    return config
+    return newconfig
 
 def merge(parent, child):
     result = copy.copy(parent)
