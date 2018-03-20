f4d473b2de93d40472b7d0850b975c6322d8dfef
James Rising <jarising@gmail.com>
2018-03-20 06:05:52 -0700

    data for new pattern models

--- climate/pattern_matching.py
+++ climate/pattern_matching.py
@@ -26,3 +26,33 @@ rcp_models = {
         ('pattern31', 'CanESM2'),
         ('pattern32', 'GFDL-CM3'),
         ('pattern33', 'CanESM2')]}}
+
+rcp_models_new = {
+    'rcp45': {
+        'surrogate_CanESM2_89': 'CanESM2',
+        'surrogate_CanESM2_94': 'CanESM2',
+        'surrogate_CanESM2_99': 'CanESM2',
+        'surrogate_GFDL-CM3_89': 'GFDL-CM3',
+        'surrogate_GFDL-CM3_94': 'GFDL-CM3',
+        'surrogate_GFDL-CM3_99': 'GFDL-CM3',
+        'surrogate_GFDL-ESM2G_01': 'GFDL-ESM2G',
+        'surrogate_GFDL-ESM2G_06': 'GFDL-ESM2G',
+        'surrogate_GFDL-ESM2G_11': 'GFDL-ESM2G',
+        'surrogate_MRI-CGCM3_01': 'MRI-CGCM3',
+        'surrogate_MRI-CGCM3_06': 'MRI-CGCM3',
+        'surrogate_MRI-CGCM3_11': 'MRI-CGCM3'},
+    
+    'rcp85': {
+        'surrogate_CanESM2_89': 'CanESM2',
+        'surrogate_CanESM2_94': 'CanESM2',
+        'surrogate_CanESM2_99': 'CanESM2',
+        'surrogate_GFDL-CM3_89': 'GFDL-CM3',
+        'surrogate_GFDL-CM3_94': 'GFDL-CM3',
+        'surrogate_GFDL-CM3_99': 'GFDL-CM3',
+        'surrogate_GFDL-ESM2G_01': 'GFDL-ESM2G',
+        'surrogate_GFDL-ESM2G_06': 'GFDL-ESM2G',
+        'surrogate_GFDL-ESM2G_11': 'GFDL-ESM2G',
+        'surrogate_MRI-CGCM3_01': 'MRI-CGCM3',
+        'surrogate_MRI-CGCM3_06': 'MRI-CGCM3',
+        'surrogate_MRI-CGCM3_11': 'MRI-CGCM3'}}
+
