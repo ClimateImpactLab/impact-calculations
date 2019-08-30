6bbf46c5cba02fe1f26a7d86157cbbc8c3e1757e
Brewster Malevich <bmalevich@rhg.com>
2019-08-30 11:28:50 -0700

    Add basic package setup and requirements

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(name='impactcalculations',
+      version='0.1',
+      description='Launch impact projection calculations.',
+      url='https://bitbucket.org/ClimateImpactLab/impact-calculations',
+      author='Impacts Team',
+      packages=find_packages(),
+      license='GNU v. 3',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      install_requires=['numpy', 'xarray', 'netCDF4', 'gspread', 'statsmodels',
+                        'scipy', 'oauth2client', 'impactcommon',
+                        'impactlab-tools', 'openest', 'metacsv'],
+      tests_require=['pytest'],
+      )
