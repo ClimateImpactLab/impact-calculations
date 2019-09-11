38bdbaf23cf5749216078f610981f9e6c939c58e
Brewster Malevich <bmalevich@rhg.com>
2019-09-11 10:03:03 -0700

    Root CLI name change to impreics following rising feedback

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(name='impactcalculations',
       tests_require=['pytest'],
       entry_points={
             'console_scripts': [
-                  'impactprojection = cli:impactcalculations_cli',
+                  'imperics = cli:impactcalculations_cli',
             ]
       },
       )
