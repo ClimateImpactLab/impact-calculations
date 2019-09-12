cb1aa085adb2b51e6c2f8e910efc53415bfc0a29
Brewster Malevich <bmalevich@rhg.com>
2019-09-12 14:22:23 -0700

    Initial system test for diagnostic run on single-energy

---
+++ tests/test_system.py
@@ -0,0 +1,95 @@
+"""
+System tests for impact projection system.
+
+These will likely fail if run as installed package. We make some strong
+assumptions about directory structure.
+"""
+
+
+import unittest
+import subprocess
+import tempfile
+import shutil
+import os
+import os.path
+
+import numpy as np
+import numpy.testing as npt
+import xarray as xr
+
+
+_here = os.path.abspath(os.path.dirname(__file__))
+
+
+class TestSingleEnergy(unittest.TestCase):
+    """Check diagnostic projection run for energy sector"""
+
+    def setUp(self):
+        """Pre-test setup sets self.results_nc4 to output xr.Dataset we want to check"""
+        self.results_nc4 = None
+        this_cwd = os.getcwd()
+        conf_path = os.path.join(_here, 'configs', 'single-energy.yml')
+        cmd_path = os.path.join(_here, os.pardir, 'diagnostic.sh')
+        resultspath_fragment = ['temp', 'single', 'rcp85', 'CCSM4', 'high',
+                                'SSP3',
+                                'FD_FGLS_inter_climGMFD_Exclude_all-issues_break2_semi-parametric_poly2_OTHERIND_other_energy_TINV_clim_income_spline_lininter.nc4']
+        tempdir = None
+
+        # Test and dump results in temp directory, gather results, then
+        # return to CWD and delete temp directory.
+        try:
+            tempdir = tempfile.mkdtemp()
+
+            # We need to link to configs directory as can't mock
+            # with unittest in py2.7. This is a ghetto hack workaround.
+            confdir_path = os.path.join(_here, os.pardir, 'configs')
+            os.symlink(confdir_path, os.path.join(tempdir, 'configs'))
+            # shutil.copy2(confdir_path, os.path.join(tempdir, 'configs'))
+
+            os.chdir(tempdir)
+            # !!Not secure!!
+            return_code = subprocess.call(['sh', str(cmd_path), str(conf_path)],
+                                          shell=True)
+            assert return_code == 0, 'command did not return code 0'
+
+            resultspath = os.path.join(tempdir, *resultspath_fragment)
+            self.results_nc4 = xr.open_dataset(resultspath)
+
+        finally:
+            os.chdir(this_cwd)
+            shutil.rmtree(tempdir)
+
+    def test_rebased(self):
+        """Smoke test shape & (head, tail) values of 'rebased' in results_nc4"""
+        actual = self.results_nc4['rebased'].values
+
+        goal_shape = (120, 1)
+        self.assertEqual(actual.shape, goal_shape)
+
+        goal_head = np.array([20.58283424,  108.42780304,  449.78918457])
+        goal_tail = np.array([-2572.81518555, -3596.93286133, -2827.64257812])
+        npt.assert_allclose(actual[:3, 0], goal_head, atol=1e-8, rtol=0)
+        npt.assert_allclose(actual[-3:, 0], goal_tail, atol=1e-8, rtol=0)
+
+    def test_year(self):
+        """Smoke test (head, tail) of 'year' in results_nc4"""
+        actual = self.results_nc4['year'].values
+
+        goal_shape = (120,)
+        self.assertEqual(actual.shape, goal_shape)
+
+        goal_head = np.array([1981, 1982, 1983])
+        goal_tail = np.array([2098, 2099, 2100])
+        npt.assert_array_equal(actual[:3], goal_head)
+        npt.assert_array_equal(actual[-3:], goal_tail)
+
+    def test_regions(self):
+        """Test 'regions' in results_nc4"""
+        actual = str(self.results_nc4['regions'].values.item())
+
+        goal = 'USA.14.608'
+        self.assertEqual(actual, goal)
+
+
+if __name__ == '__main__':
+    unittest.main()
