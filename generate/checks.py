ca3fb1e54de99eb349a4fb79b4dc1abaa1be4643
Brewster Malevich <bmalevich@rhg.com>
2019-10-14 20:02:34 -0700

    Fix regression from netCDF returning masked arrays, close #26

--- generate/checks.py
+++ generate/checks.py
@@ -15,7 +15,7 @@ def check_result_100years(filepath, variable='rebased', regioncount=24378):
         if values.shape[0] < 100 or values.shape[1] < regioncount:
             return False
 
-        if hasattr(values, 'mask') and values.mask[100, regioncount / 2]:
+        if hasattr(values, 'mask') and values.mask and values.mask[100, regioncount / 2]:
             return False
 
         if np.isnan(values[100, regioncount / 2]) or np.all(np.logical_or(values[100, :] == 0, np.logical_or(values[100, :] == 1, np.isnan(values[100, :])))):
