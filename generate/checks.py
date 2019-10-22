f082afe635ed85f404edbf15e1342d498baef3b6
Brewster Malevich <bmalevich@rhg.com>
2019-10-22 14:57:10 -0700

    Swap out getmask check based on rising feedback

--- generate/checks.py
+++ generate/checks.py
@@ -3,6 +3,7 @@ import numpy as np
 
 do_skip_check = False
 
+
 def check_result_100years(filepath, variable='rebased', regioncount=24378):
     if do_skip_check:
         return True
@@ -15,7 +16,7 @@ def check_result_100years(filepath, variable='rebased', regioncount=24378):
         if values.shape[0] < 100 or values.shape[1] < regioncount:
             return False
 
-        if hasattr(values, 'mask') and values.mask and values.mask[100, regioncount / 2]:
+        if np.ma.getmask(values) and values.mask[100, regioncount / 2]:
             return False
 
         if np.isnan(values[100, regioncount / 2]) or np.all(np.logical_or(values[100, :] == 0, np.logical_or(values[100, :] == 1, np.isnan(values[100, :])))):
