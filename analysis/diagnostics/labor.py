711fd45bfc6303d80179ecd60826bbb5d6892210
Brewster Malevich <bmalevich@rhg.com>
2019-10-23 09:42:22 -0700

    Refactor diagnostics for new risk group, update style docstrs

--- analysis/diagnostics/labor.py
+++ analysis/diagnostics/labor.py
@@ -8,25 +8,24 @@ The script is intended to be run as::
     python <path_to_script> <path_to_directory_of_projection_output>
 
 
-Key global variables, set at the head of this script file will need to be
-set and possibly updates. This script also assumes that a Julia shell is
-available from the run environment.
+Key variables, set at the head of this scripts main() file will need to be
+set and possibly updated.
 """
 
 import os
+
 import numpy as np
+
 import lib
 
 
-def cal_proj(year, weathers, climate_variables, polyorder, csvv):
-    """Quickly calc impact projection"""
-    year = int(year)
-    tas = np.stack([weathers[p][year] for p in climate_variables])
-    gammas = np.stack([csvv['gamma'][i] for i in range(polyorder)])
-    belowzero = csvv['gamma'][polyorder]
-    # if tas > 0: dot product, otherwise: belowzero; then take population
-    # mean.
-    out = np.where(tas[0] > 0, np.dot(gammas, tas), belowzero)
+def cal_proj(tasmax, gammas, belowzero):
+    """Quickly calc impact projection
+
+    Applies a polynomial to some climate variables.
+    """
+    # if tas > 0: dot product, otherwise: belowzero;
+    out = np.where(tasmax[0] > 0, np.dot(gammas, tasmax), belowzero)
     return out
 
 
@@ -41,16 +40,16 @@ def main(projout_dir):
     climate_variables = ['tasmax', 'tasmax-poly-2', 'tasmax-poly-3', 'tasmax-poly-4']
     weathertemplate = "/shares/gcp/climate/BCSD/hierid/popwt/daily/{variable}/{rcp}/CCSM4/{year}/1.0.nc4"
     csvvmodel = "labor_test"
-    model = "labor_test-risklow"
+    model_lowrisk = "labor_test-risklow"
     futureyear = 2050
     region = 'USA.14.608'  # 'IND.33.542.2153'
     polyorder = 4
 
     lib.show_header("The Predictors File (allcalcs):")
-    calcs = lib.get_excerpt(os.path.join(projout_dir, "labor-allcalcs-%s.csv" % model),
-                            2, region,
-                            range(2000, 2011) + [futureyear - 1, futureyear],
-                            hasmodel=False)
+    calcs_lowrisk = lib.get_excerpt(os.path.join(projout_dir, "labor-allcalcs-%s.csv" % model_lowrisk),
+                                    2, region,
+                                    range(2000, 2011) + [futureyear - 1, futureyear],
+                                    hasmodel=False)
 
     lib.show_header("Weather:")
     weathers = {}
@@ -64,24 +63,41 @@ def main(projout_dir):
     csvv = lib.get_csvv("/shares/gcp/social/parameters/labor/csvv_oct2019/%s.csvv" % csvvmodel)
 
     lib.show_header("Outputs:")
-    outputs = lib.get_outputs(os.path.join(projout_dir, model + '.nc4'), [1981, futureyear - 1, futureyear], region)
+    outputs = lib.get_outputs(os.path.join(projout_dir, model_lowrisk + '.nc4'), [1981, futureyear - 1, futureyear],
+                              region)
+
+    # Some variables we need to calc the projections below.
+    gammas_lowrisk = np.stack([csvv['gamma'][i] for i in range(polyorder)])
+    belowzero_lowrisk = csvv['gamma'][polyorder]
 
     lib.show_header("Un-rebased value in 1981 (%.12g reported)" % outputs[1981]['sum'])
-
     # Calc 1981 by hand...
-    ybar = np.mean(cal_proj(1981, weathers, climate_variables, polyorder, csvv))
+    ybar = np.mean(
+        cal_proj(tasmax=np.stack([weathers[p][1981] for p in climate_variables]),
+                 gammas=gammas_lowrisk,
+                 belowzero=belowzero_lowrisk)
+    )
     lib.show_header("Un-rebased value in 1981 (%.12g calculated)" % ybar)
 
     # Calc baseline by hand
-    lib.show_header("Baseline (%.12g reported):" % lib.excind(calcs, 2000, 'baseline'))
-    basevals = np.array([cal_proj(x, weathers, climate_variables, polyorder, csvv) for x in range(2001, 2011)])
-    basevals_bar = np.mean(basevals)
+    lib.show_header("Baseline (%.12g reported):" % lib.excind(calcs_lowrisk, 2000, 'baseline'))
+
+    # Project for baseline range and then take population mean.
+    basevals = []
+    for x in range(2001, 2011):
+        basevals.append(
+            cal_proj(tasmax=np.stack([weathers[p][x] for p in climate_variables]),
+                     gammas=gammas_lowrisk,
+                     belowzero=belowzero_lowrisk)
+        )
+    basevals_bar = np.mean(np.array(basevals))
     lib.show_header("Baseline (%.12g calculated):" % basevals_bar)
 
     # Calc rebased future value
     lib.show_header("Rebased future result (%.12g reported)" % (outputs[futureyear]['rebased']))
-    ybar_future = cal_proj(futureyear, weathers, climate_variables, polyorder,
-                           csvv)
+    ybar_future = cal_proj(tasmax=np.stack([weathers[p][futureyear] for p in climate_variables]),
+                           gammas=gammas_lowrisk,
+                           belowzero=belowzero_lowrisk)
     yrebase = np.mean(ybar_future) - basevals_bar
     lib.show_header("Rebased future result (%.12g calculated)" % yrebase)
 
