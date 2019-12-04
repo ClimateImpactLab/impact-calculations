a85e271bd4cdb27c254f4ce6226628c0d1f25c40
Brewster Malevich <bmalevich@rhg.com>
2019-12-03 23:29:23 -0800

    Add system test for energy aggregation, close #14

--- tests/test_system.py
+++ tests/test_system.py
@@ -1,8 +1,9 @@
 """
 System tests for impact projection system.
 
-These will likely fail if run as installed package. We make some strong
-assumptions about directory structure.
+These will likely fail if run as installed package. We make strong
+assumptions about directory structure. These are also smoke tests,
+and are not comprehensive.
 """
 
 
@@ -10,6 +11,7 @@ import unittest
 import subprocess
 import os
 import os.path
+from glob import glob
 
 import numpy as np
 import numpy.testing as npt
@@ -51,10 +53,10 @@ class TestSingleEnergy(unittest.TestCase):
         resultspath_fragment = ['temp', 'single', 'rcp85', 'CCSM4', 'high',
                                 'SSP3',
                                 'FD_FGLS_inter_climGMFD_Exclude_all-issues_break2_semi-parametric_poly2_OTHERIND_other_energy_TINV_clim_income_spline_lininter.nc4']
-        
+
         os.chdir(os.path.join(_here, os.pardir))
         try:
-            # This is going to *clobber* anything in the 
+            # This is going to *clobber* anything in the
             # "impact-calculations/temp" directory.
 
             # !!Not secure!!
@@ -119,7 +121,7 @@ class TestSingleMortality(unittest.TestCase):
 
         os.chdir(os.path.join(_here, os.pardir))
         try:
-            # This is going to *clobber* anything in the 
+            # This is going to *clobber* anything in the
             # "impact-calculations/temp" directory.
 
             # !!Not secure!!
@@ -199,7 +201,7 @@ class TestMonteCarloEnergy(unittest.TestCase):
 
             # Call external shell script as hack to get around control returning to the
             # test too early.
-            return_code = subprocess.call(['sh', 'tests/testmontecarloenergy.sh', 
+            return_code = subprocess.call(['sh', 'tests/testmontecarloenergy.sh',
                                            str(conf_path)])
             assert return_code == 0, 'command did not return code 0'
 
@@ -292,3 +294,88 @@ class TestMonteCarloEnergy(unittest.TestCase):
 
         goal = 'USA.14.608'
         self.assertEqual(actual, goal)
+
+
+@pytest.mark.imperics_shareddir
+class TestAggregateEnergy(unittest.TestCase):
+    """Check projection aggregation for energy sector
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """Pre-test setup sets cls.results_* to output xr.Dataset we want to check"""
+        cls.results_aggregated = None
+        cls.results_levels = None
+        # This is a hack because the projection run scripts can only be launched
+        # from the root of the impact-calculations directory.
+        # I don't have a way around this as py2.7 unittest doesn't have mocks.
+        this_cwd = os.getcwd()
+        conf_path = os.path.abspath(os.path.join(_here, 'configs', 'aggregate-energy.yml'))
+        cmd_path = 'aggregate.sh'  # Must be run in PWD
+
+        resultspath_fragment = ['median', 'rcp45', 'surrogate_CanESM2_89', 'low', 'SSP3',
+                                'FD_FGLS_inter_climGMFD_Exclude_all-issues_break2_semi-parametric_poly2_OTHERIND_other_energy_TINV_clim_income_spline-incadapt-withprice']
+        # Output files have these fragments tagged on the end.
+
+        # Remove existing aggregation files in output directory!
+        # These are files matching glob *aggregated.nc4 and *levels.nc4!
+        with open(conf_path) as fl:
+            cfg = yaml.load(fl, Loader=yaml.SafeLoader)
+        cfg_outpath = cfg['outputdir']
+        # cfg_basename = cfg['basename']
+        for ending in ['aggregated', 'levels']:
+            glob_pattern = os.path.join(*[cfg_outpath] + resultspath_fragment[:-1] + ['*' + ending + '.nc4'])
+            for target in glob(glob_pattern):
+                print('removing existing ' + target)
+                os.remove(target)
+
+        os.chdir(os.path.join(_here, os.pardir))
+        try:
+            # !!Not secure!!
+            return_code = subprocess.call(['sh', str(cmd_path), str(conf_path)])
+            assert return_code == 0, 'command did not return code 0'
+
+            resultspath = os.path.join(*[cfg_outpath] + resultspath_fragment[:-1] + [resultspath_fragment[-1] + '-aggregated.nc4'])
+            cls.results_aggregated = xr.open_dataset(resultspath)
+
+            resultspath = os.path.join(*[cfg_outpath] + resultspath_fragment[:-1] + [resultspath_fragment[-1] + '-levels.nc4'])
+            cls.results_levels = xr.open_dataset(resultspath)
+
+        finally:
+            os.chdir(this_cwd)
+
+    def test_levels_regions(self):
+        """Test regions in *levels results file"""
+        actual = self.results_levels['regions'].values
+        self.assertEqual(actual.shape, (24378, ))
+        self.assertEqual(actual[0], u'CAN.1.2.28')
+        self.assertEqual(actual[-1], u'BWA.4.13')
+
+    def test_levels_rebased(self):
+        """Test shape & (head, tail) values of 'rebased' in *levels file"""
+        actual = self.results_levels['rebased'].values
+
+        self.assertEqual(actual.shape, (119, 24378))
+
+        goal_head = np.array([7.7720156, 28.00633, 19.375658])
+        goal_tail = np.array([-602.15814, -619.549, -649.28516])
+        npt.assert_allclose(actual[:3, 0], goal_head, atol=1e-4, rtol=0)
+        npt.assert_allclose(actual[-3:, -1], goal_tail, atol=1e-4, rtol=0)
+
+    def test_aggregated_regions(self):
+        """Test regions in *aggregated results file"""
+        actual = self.results_aggregated['regions'].values
+        self.assertEqual(actual.shape, (5716, ))
+        self.assertEqual(actual[0], u'CAN.1.2.28')
+        self.assertEqual(actual[-1], u'BWA.4.13')
+
+    def test_aggregated_rebased(self):
+        """Test shape & (head, tail) values of 'rebased' in *aggregated file"""
+        actual = self.results_aggregated['rebased'].values
+
+        self.assertEqual(actual.shape, (119, 5716))
+
+        goal_head = np.array([2.8525314, 2.285627, 2.2759197])
+        goal_tail = np.array([-42.953907, -65.55747, -51.37832])
+        npt.assert_allclose(actual[:3, 0], goal_head, atol=1e-4, rtol=0)
+        npt.assert_allclose(actual[-3:, -1], goal_tail, atol=1e-4, rtol=0)
