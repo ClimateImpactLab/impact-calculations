55290bf83d86683f6f641cc3cdda778e86401e36
Brewster Malevich <bmalevich@rhg.com>
2020-02-03 19:16:59 -0800

    Fill in prelim docstrings in datastore.spacetime

--- datastore/spacetime.py
+++ datastore/spacetime.py
@@ -63,7 +63,8 @@ class SpaceTimeLoadedData(SpaceTimeData):
         self.adm3fallback = adm3fallback
             
     def get_time(self, region):
-        """Return a timeseries vector for the given region."""
+        """Return a timeseries vector for the given region
+        """
         ii = self.indices.get(region, None)
         if ii is None:
             if self.adm3fallback and len(region) > 3:
@@ -78,14 +79,47 @@ class SpaceTimeLoadedData(SpaceTimeData):
         return self
 
 class SpaceTimeLazyData(SpaceTimeData):
+    """SpaceTimeData subclass given `get_time` when initialized
+
+    Parameters
+    ----------
+    year0 : int
+        The first year of the data
+    year1 : int
+        The last year of the data (inclusive of year1)
+    regions : sequence of str
+        Region names are generally drawn from the hierarchy.
+    get_time : Callable
+        Callable taking a single input argument, usually a str region name.
+    """
     def __init__(self, year0, year1, regions, get_time):
         super(SpaceTimeLazyData, self).__init__(year0, year1, regions)
         self.get_time = get_time
 
     def get_time(self, region):
+        """Get timeseries for a region
+        
+        Pass `region` to `self.get_time` and return output"""
         return self.get_time(region)
 
 class SpaceTimeProductData(SpaceTimeData):
+    """Create a SpaceTime as the product of two SpaceTimeData-likes
+
+    Parameters
+    ----------
+    year0 : int
+        The first year of the data
+    year1 : int
+        The last year of the data (inclusive of year1)
+    regions : sequence of str
+        Region names are generally drawn from the hierarchy.
+    spdata1 : SpaceTimeData-like
+    spdata2 : SpaceTimeData-like
+    combiner : Callable, optional
+        Callable taking two args, the output of `*.get_time()` called from
+        `spdata1` and `spdata2`. By default, returns the product of the two
+        timeseries.
+    """
     def __init__(self, year0, year1, regions, spdata1, spdata2, combiner=lambda x, y: x * y):
         super(SpaceTimeProductData, self).__init__(year0, year1, regions)
         self.spdata1 = spdata1
@@ -93,6 +127,8 @@ class SpaceTimeProductData(SpaceTimeData):
         self.combiner = combiner
 
     def get_time(self, region):
+        """Return timeseries for a region
+        """
         datum1 = self.spdata1.get_time(region)
         datum2 = self.spdata2.get_time(region)
         if datum1 is None:
@@ -102,18 +138,49 @@ class SpaceTimeProductData(SpaceTimeData):
         return self.combiner(datum1, datum2)
 
 class SpaceTimeBipartiteData(SpaceTimeData):
-    """Loads the historical data in __init__, and the future data in load()."""
-    
+    """ Abstract class for loading historical and future data in two steps
+
+    Loads the historical data in __init__, and the future data in load().
+    """
     def load(self, year0, year1, model, scenario):
         """Return a SpaceTimeData for the given model and scenario."""
         raise NotImplementedError
 
 class SpaceTimeBipartiteFromProviderData(SpaceTimeData):
+    """SpaceTimeBipartite from Providers
+
+    Parameters
+    ----------
+    Provider : impactcommon.exogenous_economy.provider.BySpaceProvider-like or impactcommon.exogenous_economy.provider.BySpaceTimeProvider-like
+    year0 : int
+        The first year of the data
+    year1 : int
+        The last year of the data (inclusive of year1)
+    regions : sequence of str
+        Region names are generally drawn from the hierarchy.
+    """
     def __init__(self, Provider, year0, year1, regions):
         super(SpaceTimeBipartiteFromProviderData, self).__init__(year0, year1, regions)
         self.Provider = Provider
 
     def load(self, year0, year1, model, scenario):
+        """Load data from `self.Provider`
+
+        Parameters
+        ----------
+        year0 : int
+            The first year of the data.
+        year1 : int
+            The last year of the data (inclusive of year1).
+        model : str
+            Model (IAM) to draw from.
+        scenario : str
+            Scenario (SSP) to use.
+
+        Returns
+        -------
+        SpaceTimeLazyData
+        """
         provider = self.Provider(model, scenario)
         return SpaceTimeLazyData(year0, year1, self.regions, lambda region: self.adjustlen(year0, year1, provider.get_startyear(),
                                                                                            provider.get_timeseries(region)))
@@ -136,6 +203,23 @@ class SpaceTimeBipartiteFromProviderData(SpaceTimeData):
         return prepared
             
 class SpaceTimeProductBipartiteData(SpaceTimeBipartiteData):
+    """Create a SpaceTimeBipartite as the product of two SpaceTimeBipartiteData-likes
+
+    Parameters
+    ----------
+    year0 : int
+        The first year of the data
+    year1 : int
+        The last year of the data (inclusive of year1)
+    regions : sequence of str
+        Region names are generally drawn from the hierarchy.
+    spdata1 : SpaceTimeBipartiteData-like
+    spdata2 : SpaceTimeBipartiteData-like
+    combiner : Callable, optional
+        Callable taking two args, the output of `*.get_time()` called from
+        `spdata1` and `spdata2`. By default, returns the product of the two
+        timeseries.
+    """
     def __init__(self, year0, year1, regions, spdata1, spdata2, combiner=lambda x, y: x * y):
         super(SpaceTimeProductBipartiteData, self).__init__(year0, year1, regions)
         self.spdata1 = spdata1
@@ -143,6 +227,23 @@ class SpaceTimeProductBipartiteData(SpaceTimeBipartiteData):
         self.combiner = combiner
 
     def load(self, year0, year1, model, scenario):
+        """Load data from `self.Provider`
+
+        Parameters
+        ----------
+        year0 : int
+            The first year of the data.
+        year1 : int
+            The last year of the data (inclusive of year1).
+        model : str
+            Model (IAM) to draw from.
+        scenario : str
+            Scenario (SSP) to use.
+
+        Returns
+        -------
+        SpaceTimeProductData
+        """
         return SpaceTimeProductData(year0, year1, self.regions, self.spdata1.load(year0, year1, model, scenario), self.spdata2.load(year0, year1, model, scenario), self.combiner)
 
 class SpaceTimeUnipartiteData(SpaceTimeBipartiteData):
@@ -154,28 +255,62 @@ class SpaceTimeUnipartiteData(SpaceTimeBipartiteData):
         return self.loader(year0, year1, self.regions, model, scenario)
 
 class SpaceTimeConstantData(SpaceTimeBipartiteData):
+    """SpaceTimeBipartiteData using constant data
+    """
     def __init__(self, constant):
         super(SpaceTimeConstantData, self).__init__(-np.inf, np.inf, None)
         self.constant = constant
 
     def get_time(self, region):
+        """Returns `self.constant`, regardless of input
+        """
         return self.constant
 
     def load(self, year0, year1, model, scenario):
+        """Returns `self`, regardless of input
+        """
         return self
     
 class SpaceTimeSpatialOnlyData(SpaceTimeBipartiteData):
+    """SpaceTimeBipartiteData with only spatial (region) mapping available
+
+    Parameters
+    ----------
+    mapping : dict
+        {region-key: value} mapping.
+    """
     def __init__(self, mapping):
         super(SpaceTimeSpatialOnlyData, self).__init__(-np.inf, np.inf, mapping.keys())
         self.mapping = mapping
 
     def get_time(self, region):
+        """Return a timeseries vector for the given region
+        """
         return self.mapping[region]
 
     def load(self, year0, year1, model, scenario):
+        """Returns `self` regardless of input
+        """
         return self
 
 class SpaceTimeMatrixData(SpaceTimeData):
+    """SpaceTimeData-like for 2D np.matrix data
+
+    Parameters
+    ----------
+    year0 : int
+        The first year of the data
+    year1 : int
+        The last year of the data (inclusive of year1)
+    regions : sequence of str
+        Region names are generally drawn from the hierarchy.
+    array : array_like
+        Should have (year1 - year0 + 1) rows and len(regions) columns.
+    ifmissing : None or 'mean' or 'logmean'
+        When a region is not in the data, return None or the mean or logmean timeseries.
+    adm3fallback : bool
+        If True, when a region is not in the data, instead the 3-character ADM0 suffix will be queried.
+    """
     def __init__(self, year0, year1, regions, array, ifmissing=None, adm3fallback=False):
         super(SpaceTimeMatrixData, self).__init__(year0, year1, regions)
         self.array = array # as YEAR x REGION
@@ -183,6 +318,22 @@ class SpaceTimeMatrixData(SpaceTimeData):
         self.adm3fallback = adm3fallback
 
     def load(self, year0, year1, model, scenario):
+        """
+        Parameters
+        ----------
+        year0 : int
+            The first year of the data.
+        year1 : int
+            The last year of the data (inclusive of year1).
+        model : str
+            Model (IAM) to draw from.
+        scenario : str
+            Scenario (SSP) to use.
+
+        Returns
+        -------
+        SpaceTimeLoadedData
+        """
         if year0 == self.year0 and year1 == self.year1:
             array = self.array
         else:
