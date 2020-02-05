3c745cc07328c27f96bbb25b6a6ae3f95f750296
Brewster Malevich <bmalevich@rhg.com>
2020-02-05 13:59:52 -0800

    Add support funcs to check hierid membership

--- datastore/irregions.py
+++ datastore/irregions.py
@@ -1,6 +1,46 @@
 import csv
+from pandas import read_csv
 from impactlab_tools.utils import files
 import helpers.header as headre
+
+def contains_region(parents, candidate, hierid_df):
+    """Test if parent region contains candidate region
+
+    Parameters
+    ----------
+    parents : str or Sequence of str
+        Parent region(s).
+    candidate : str
+        Region to test if within `parents`.
+    hierid_df : pandas.core.frame.DataFrame
+        DataFrame of hierarchical region relationships. Must have columns 
+        'region-key', 'parent-key', populated with str.
+
+    Returns
+    -------
+    bool
+    """
+    # This may be candidate for lru_cache once python version upgraded.
+    if isinstance(parents, str):
+        parents = [parents]
+    candidate = str(candidate)
+
+    try:
+        parent_key = (hierid_df
+                      .loc[hierid_df.loc[:, "region-key"] == candidate, "parent-key"]
+                      .values.item())
+    except ValueError:  # No parent_key, so at trunk of tree.
+        return False
+
+    if parent_key in parents:
+        return True
+
+    return contains_region(parents, parent_key, hierid_df)
+
+def read_hierid(csvpath):
+    """Read hierarchy.csv hierid file from path, return pandas.DataFrame
+    """
+    return read_csv(csvpath, skiprows=31)
 
 def load_regions(hierarchy, dependencies):
     """Load the rows of hierarchy.csv associated with all known regions."""
