ab40032c4707abe63304eeaf9a7071773b539554
Brewster Malevich <bmalevich@rhg.com>
2020-02-25 11:49:13 -0800

    Fix from idx np by float, annoying exception print

--- generate/checks.py
+++ generate/checks.py
@@ -7,7 +7,7 @@ do_skip_check = False
 def check_result_100years(filepath, variable='rebased', regioncount=24378):
     if do_skip_check:
         return True
-    
+
     try:
         rootgrp = Dataset(filepath, 'r', format='NETCDF4')
         values = rootgrp.variables[variable][:, :]
@@ -16,12 +16,14 @@ def check_result_100years(filepath, variable='rebased', regioncount=24378):
         if values.shape[0] < 100 or values.shape[1] < regioncount:
             return False
 
-        if np.ma.getmask(values) and values.mask[100, regioncount / 2]:
+        if np.ma.getmask(values) and values.mask[100, int(regioncount / 2)]:
             return False
 
-        if np.isnan(values[100, regioncount / 2]) or np.all(np.logical_or(values[100, :] == 0, np.logical_or(values[100, :] == 1, np.isnan(values[100, :])))):
+        if np.isnan(values[100, int(regioncount / 2)]) or np.all(np.logical_or(values[100, :] == 0, np.logical_or(values[100, :] == 1, np.isnan(values[100, :])))):
             return False
 
         return True
-    except:
+    except Exception as ex:
+        print("Exception raised but returning anyways:")
+        print(ex)
         return False
