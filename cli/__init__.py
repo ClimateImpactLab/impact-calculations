4b46c5979e625d118ef4b36d0485c98c28d54f2e
Brewster Malevich <bmalevich@rhg.com>
2020-03-12 15:41:43 -0700

    Add CLI unit tests

--- cli/__init__.py
+++ cli/__init__.py
@@ -4,54 +4,58 @@
 import os
 import click
 from impactlab_tools.utils.files import get_file_config
-import generate.generate as gg
-import generate.aggregate as ga
+from generate.generate import main as ggmain
+from generate.aggregate import main as gamain
 
 
 # This is your main entry point
-@click.group(context_settings={'help_option_names': ['-h', '--help']})
+@click.group(context_settings={"help_option_names": ["-h", "--help"]})
 def impactcalculations_cli():
     """Launch impact projection calculations"""
 
 
-@impactcalculations_cli.command(
-    help='Post-process aggregation of impact projections'
-)
-@click.argument('confpath', type=click.Path())
+@impactcalculations_cli.command(help="Post-process aggregation of impact projections")
+@click.argument("confpath", type=click.Path())
 def aggregate(confpath):
     """Run the impact projection aggregation system with configuration file"""
     file_configs = get_file_config(confpath)
-    ga.main(file_configs)
+    gamain(file_configs)
 
 
 @impactcalculations_cli.command(
-    help='Generate impact projections from configuration file'
+    help="Generate impact projections from configuration file"
 )
-@click.argument('confpath', type=click.Path())
-@click.option('-c', '--conf', nargs=1, default='', multiple=True,
-              help='Additional KEY=VALUE configuration option.')
+@click.argument("confpath", type=click.Path())
+@click.option(
+    "-c",
+    "--conf",
+    nargs=1,
+    default="",
+    multiple=True,
+    help="Additional KEY=VALUE configuration option.",
+)
 def generate(confpath, conf):
     """Run the impact projection generate system with configuration file"""
     file_configs = get_file_config(confpath)
-    arg_configs = dict(arg.split('=') for arg in conf)
+    arg_configs = dict(arg.strip().split("=") for arg in conf)
     file_configs.update(arg_configs)
-    gg.main(file_configs, str(confpath))
+    ggmain(file_configs, str(confpath))
 
 
 @impactcalculations_cli.command(
-    help='Generate diagnostic impact projection run from configuration file'
+    help="Generate diagnostic impact projection run from configuration file"
 )
-@click.argument('confpath', type=click.Path())
+@click.argument("confpath", type=click.Path())
 def diagnostic(confpath):
     """Run the impact projection diagnostic system with configuration path"""
     file_configs = get_file_config(confpath)
 
     diagnostic_configs = {
-        'filter-region': 'USA.14.608',
-        'outputdir': os.path.join(os.getcwd(), 'temp'),  # PWD/temp
-        'singledir': 'single',
-        'mode': 'writecalcs',
+        "filter-region": "USA.14.608",
+        "outputdir": os.path.join(os.getcwd(), "temp"),  # PWD/temp
+        "singledir": "single",
+        "mode": "writecalcs",
     }
 
     file_configs.update(diagnostic_configs)
-    gg.main(file_configs, str(confpath))
+    ggmain(file_configs, str(confpath))
