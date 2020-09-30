055dd5ece797e5bdd823256379bc2741c33f7c72
Brewster Malevich <bmalevich@rhg.com>
2020-09-30 11:47:05 -0700

    Minor cleanup; drop unused import

--- interpret/specification.py
+++ interpret/specification.py
@@ -11,7 +11,7 @@ precipitation.
 import re
 from adaptation import csvvfile, curvegen, curvegen_known, curvegen_arbitrary, covariates, constraints
 from datastore import irvalues
-from openest.generate import smart_curve, selfdocumented
+from openest.generate import smart_curve
 from openest.curves.smart_linextrap import LinearExtrapolationCurve
 from openest.generate.stdlib import *
 from impactcommon.math import minpoly, minspline
