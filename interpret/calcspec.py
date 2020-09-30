206db1a6d4fed2c7fd66a2f476ecf34d97319157
Brewster Malevich <bmalevich@rhg.com>
2020-09-30 11:25:17 -0700

    Fix clipping fail with filter-region config

--- interpret/calcspec.py
+++ interpret/calcspec.py
@@ -80,10 +80,18 @@ def prepare_interp_raw(csvv, weatherbundle, economicmodel, qvals, farmer="full",
                 new_unit = str(reunit_spec["new-unit"])
                 this_csvv["variables"][target_variable]["unit"] = new_unit
 
+        # Subset to regions (i.e. hierids) to act on if config has
+        # filter-region.
+        filter_region = config.get("filter-region")
+        if filter_region:
+            target_regions = [str(filter_region)]
+        else:
+            target_regions = weatherbundle.regions
+
         model = specification.create_curvegen(
             this_csvv,
             covariator,
-            weatherbundle.regions,
+            target_regions,
             farmer=farmer,
             specconf=modelspecconf,
         )
