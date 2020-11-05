1e2bfb55cf5b992d5361a1d738231434920126d8
James Rising <jarising@gmail.com>
2020-11-05 10:07:52 -0800

    working unit tests

--- tests/test_season_conditionals.py
+++ tests/test_season_conditionals.py
@@ -1,5 +1,18 @@
+"""Try different representations of conditional month-of-season coefficients.
+
+All of these models are based on the core maize model and the
+precipitation polynomial, which was had the original form of the
+month-of-season processing.
+
+"""
+
+import pytest
+import yaml, copy
+import numpy as np
+from impactlab_tools.utils import files
+from adaptation import csvvfile
 from interpret import container
-from generate import loadmodels
+from generate import loadmodels, pvalses, caller, effectset
 
 master_config = """
 timerate: month
@@ -66,8 +79,8 @@ def modify_config_triangle(config):
     """
 
     config = copy.deepcopy(config)
-    del config['models'][0]['specification']['precip']['suffixes']
-    config['models'][0]['specification']['precip']['suffix-triangle'] = json.loads(json_str)
+    del config['models'][0]['specifications']['precip']['suffixes']
+    config['models'][0]['specifications']['precip']['suffix-triangle'] = yaml.load(json_str)
     return config
 
 def modify_config_condition(config):
@@ -82,26 +95,58 @@ def modify_config_condition(config):
     
     config = copy.deepcopy(config)
     
-    precip_config = config['models'][0]['specification'].pop('precip')
+    precip_config = config['models'][0]['specifications'].pop('precip')
     precip1_config = copy.deepcopy(precip_config)
     precip1_config['suffixes'] = [1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     precip2_config = copy.deepcopy(precip_config)
     precip2_config['suffixes'] = [0, 0, 0, 4, 5, 6, 7, 8, 9, 'r', 'r', 'r']
     
-    config['models'][0]['specification']['precip1'] = precip1_config
-    config['models'][0]['specification']['precip2'] = precip2_config
-    config['models'][0]['calculation'][0]['Sum'] = json.loads(json_str)
+    config['models'][0]['specifications']['precip1'] = precip1_config
+    config['models'][0]['specifications']['precip2'] = precip2_config
+    config['models'][0]['calculation'][0]['Sum'] = yaml.load(json_str)
     return config
 
-config = json.loads(master_config)
-config['mode'] = 'diagnostic'
-config['region'] = 'USA.14.608'
+module = 'interpret.calcspec'
 
-container.preload()
+@pytest.fixture
+def setup():
+    """Run projection with default config and save elements."""
+    config = yaml.load(master_config)
 
-clim_scenario, clim_model, weatherbundle, econ_scenario, econ_model, economicmodel = loadmodels.single(mod.get_bundle_iterator(config))
-pvals = pvalses.ConstantPvals(.5)
+    container.preload()
 
-outputs = container.produce(None, weatherbundle, economicmodel, pvals, config)
+    clim_scenario, clim_model, weatherbundle, econ_scenario, econ_model, economicmodel = loadmodels.single(container.get_bundle_iterator(config))
+    pvals = pvalses.ConstantPvals(.5)
+    csvv = csvvfile.read(files.configpath(config['models'][0]['csvvs']))
+    
+    calculation, dependencies, baseline_get_predictors = caller.call_prepare_interp(csvv, module, weatherbundle, economicmodel, pvals["full"], specconf=config['models'][0], config=config, standard=False)
+    outputs_master = effectset.small_print(weatherbundle, calculation, ['USA.14.608'])
 
-print(outputs)
+    return dict(config=config, weatherbundle=weatherbundle, economicmodel=economicmodel, pvals=pvals, csvv=csvv, outputs=outputs_master)
+
+def test_triangle(setup):
+    """Try to use a triangle suffixes config equivalent to the list suffixes."""
+    config_triangle = modify_config_triangle(setup['config'])
+    weatherbundle = setup['weatherbundle']
+    economicmodel = setup['economicmodel']
+    pvals = setup['pvals']
+    csvv = setup['csvv']
+
+    calculation, dependencies, baseline_get_predictors = caller.call_prepare_interp(csvv, module, weatherbundle, economicmodel, pvals["full"], specconf=config_triangle['models'][0], config=config_triangle, standard=False)
+    outputs_triangle = effectset.small_print(weatherbundle, calculation, ['USA.14.608'])
+
+    np.testing.assert_array_almost_equal(setup['outputs'], outputs_triangle)
+
+def test_condition(setup):
+    """Try to use two predictors conditionally, equivalent to the single predictor."""
+    config_condition = modify_config_condition(setup['config'])
+    weatherbundle = setup['weatherbundle']
+    economicmodel = setup['economicmodel']
+    pvals = setup['pvals']
+    csvv = setup['csvv']
+
+    calculation, dependencies, baseline_get_predictors = caller.call_prepare_interp(csvv, module, weatherbundle, economicmodel, pvals["full"], specconf=config_condition['models'][0], config=config_condition, standard=False)
+    outputs_condition = effectset.small_print(weatherbundle, calculation, ['USA.14.608'])
+
+    np.testing.assert_array_almost_equal(setup['outputs'], outputs_condition)
+
