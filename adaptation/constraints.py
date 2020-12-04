f5eee36df8421a65d1c103687b92ee4d9a686363
Brewster Malevich <bmalevich@rhg.com>
2020-12-04 12:38:48 -0800

    Minor style cleanup

--- adaptation/constraints.py
+++ adaptation/constraints.py
@@ -5,7 +5,8 @@ beta-clipping requires no complicate functions and U-clipping has been
 moved to open-estimate.
 """
 
-import csv, os
+import csv
+import os
 import numpy as np
 from generate import caller
 
@@ -20,6 +21,7 @@ We want to ensure that
 For each x, f(x | gdppc_t) against f(x | gdppc_0) and take the better of the two.
 """
 
+
 def make_get_coeff_goodmoney(weatherbundle, covariator, curvegen, baselinemins, curve_get_coeff, flipsign=False):
     # Record all baseline log GDP p.c.
     baseline_loggdppc = {}
@@ -28,7 +30,9 @@ def make_get_coeff_goodmoney(weatherbundle, covariator, curvegen, baselinemins, 
 
     # Get marginals on loggdppc (assumed to be constant)
     loggdppc_marginals = curvegen.get_marginals('loggdppc')
-    loggdppc_marginals = np.array([loggdppc_marginals[predname] for predname in curvegen.prednames]) # same order as temps
+    loggdppc_marginals = np.array(
+        [loggdppc_marginals[predname] for predname in curvegen.prednames]  # same order as temps
+    )
 
     # flipsign = True when subtracting off baseline
     signcoeff = -1 if flipsign else 1
@@ -43,15 +47,18 @@ def make_get_coeff_goodmoney(weatherbundle, covariator, curvegen, baselinemins, 
             return curve_get_coeff(curve)
     return coeff_getter
 
+
 def get_curve_minima(regions, curvegen, covariator, mint, maxt, analytic):
     # Determine minimum value of curve between mint and maxt
     print("Determining minimum temperatures.")
     return get_curve_extrema(regions, curvegen, covariator, mint, maxt, analytic, 'boatpose', 'minpath')
 
+
 def get_curve_maxima(regions, curvegen, covariator, mint, maxt, analytic):
     # Determine maximum value of curve between mint and maxt
     print("Determining maximum temperatures.")
     return get_curve_extrema(regions, curvegen, covariator, mint, maxt, analytic, 'downdog', 'maxpath')
+
 
 def get_curve_extrema(regions, curvegen, covariator, mint, maxt, analytic, direction, extpathkey):
     baselinecurves = {}
