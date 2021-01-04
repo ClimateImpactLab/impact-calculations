3e93af103be157151f8d1c94c895353918ca9e6f
Emile Tenezakis <etenezakis@battuta.berkeley.edu>
2021-01-03 23:26:35 -0800

    some run

--- apply_aggregate.sh
+++ apply_aggregate.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 source ${AGPROJ}/utils/projection_variables.sh
 
-for crop in soy; do
+for crop in cassava; do
 
-	dir=${medians[${crop}]}
+	dir=${montecarlos[${crop}]}
 	base=${bases[${crop}]}
 	vars=${variables[${crop}]}
 	
 	python -m generate.apply_aggregate \
-	/shares/gcp/outputs/agriculture/impacts-mealy/${dir} \
+	/shares/gcp/outputs/agriculture/impacts-mealy/${dir}/montecarlo \
 	/mnt/CIL_agriculture/4_outputs/3_projections/6_value_checks/missing_aggregated_${dir}.csv \
 	constcsv/estimation/agriculture/Data/1_raw/3_cropped_area/agglomerated-world-new-hierid-crop-weights.csv:hierid:${crop} \
 	$vars  \
