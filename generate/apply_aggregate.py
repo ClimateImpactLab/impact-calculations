46d51485eaa890b09c23fe37b7083010e6ed7175
Emile Tenezakis <etenezakis@sacagawea.berkeley.edu>
2021-03-04 01:29:27 -0800

    config style for apply_aggregate

--- generate/apply_aggregate.py
+++ generate/apply_aggregate.py
@@ -1,3 +1,18 @@
+"""
+This code should be run from the command line with a config yml file. The yml file should contain (most are identical to the aggregation.py parameters) : 
+    - outputdir
+    - targetdirs : the path to a csv file that lists the target directories to run
+    - weighting
+    - only_variables
+    - basename
+    - processes : number of processes to run in parallel. Each process takes care of one target directory.
+
+Run this code that way : 
+
+python -m generate.apply_aggregate {config.yml}
+
+"""
+
 import sys
 import glob
 import multiprocessing
@@ -5,9 +20,13 @@ import csv
 import pandas as pd
 import os
 from itertools import product
+from joblib import Parallel, delayed
 
 def ParseVariables(variables):
 
+    """Turns a list of variables to a string that can be used as the 'only-variables' parameter to the aggregation code through a command line
+    execution
+    """
     Parser = "["
     i = 0
     for var in variables:
@@ -23,7 +42,8 @@ def ParseVariables(variables):
 
 def DoAggregate(outputdir, targetdir, weighting, only_variables, basename):
 
-
+    """Sends a command line python execution of the aggregation code. 
+    """
     os.system('python -m generate.aggregate' + 
               ' --outputdir=' + outputdir + 
               ' --targetdir=' + targetdir + 
@@ -33,16 +53,16 @@ def DoAggregate(outputdir, targetdir, weighting, only_variables, basename):
     
 
 def ReadTargets(file):
-    """Reads a csv file containing a single column : the full path of a target directory.
+    """Reads a csv file containing a single column and > 0 number of rows, each containing the full path of a target directory.
 
     Parameters
     ----------
-    file : str - full path to a target dir, up to the SSP, included.
+    file : str - full path to a target dir, up to the SSP included.
 
     Requires
     -------
     file :  the list of paths must not contain duplicate, and start from the root, without a last trailing slash. for example :
-    '/mnt/battuta_shares/gcp/outputs/agriculture/impacts-mealy/rice-median-291020/median/rcp85/surrogate_MRI-CGCM3_11/low/SSP3/'
+    '/mnt/battuta_shares/gcp/outputs/agriculture/impacts-mealy/rice-median-291020/median/rcp85/surrogate_MRI-CGCM3_11/low/SSP3'
 
     Returns
     -------
@@ -51,17 +71,23 @@ def ReadTargets(file):
 
     return pd.read_csv(file)['targetdir'].tolist()
 
-if __name__ == "__main__":  
-    """Runs an aggregation over a list of targetdirs
-    """
-    outputdir=sys.argv[1]
-    targets=ReadTargets(sys.argv[2])
-    weighting=sys.argv[3]
-    only_variables=ParseVariables(sys.argv[4].split(','))
-    basename=sys.argv[5]
-    for target in targets:
-        print("doing " + target)
-        DoAggregate(outputdir, target, weighting, only_variables, basename)
+if __name__ == "__main__": 
 
-    # with multiprocessing.Pool(processes=nb_processes) as pool:
-    #     pool.starmap(DoAggregate, product(outputdir, targets, weighting, only_variables, basename))
+    """Runs an aggregation over a list of targetdirs defined in a csv file. Runs with a config.
+    """
+
+    import yaml
+    with open(str(sys.argv[1]), 'r') as file:
+        run_config = yaml.load(file)
+
+    assert all(x in config for x in ['outputdir','targetdirs', 'weighting', 'only_variables','basename','processes']), 'incomplete configurations file'
+
+    outputdir = config.get('outputdir')
+    targetdirs= ReadTargets(config.get('targetdirs'))
+    weighting= config.get('weighting')
+    only_variables= ParseVariables(config.get('only-variables'))
+    basename= config.get('basename')
+    processes= int(config.get('processes'))
+
+    with Parallel(n_jobs=nprocesses) as parallelize:
+        parallelize(delayed(DoAggregate)(outputdir, target, weighting, only_variables, basename) for target in targets) 
