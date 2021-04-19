9be309ef5bff87c1e2739c62af7aa62ced847cf4
Brewster Malevich <bmalevich@rhg.com>
2021-04-19 23:55:24 +0000

    Remove unused monkeypatch arg in test

--- tests/test_covariates.py
+++ tests/test_covariates.py
@@ -55,7 +55,7 @@ def get_fractional_change(scalar=1, baseline_year=2050, future_year=2051):
     return fractional_change
 
 @pytest.mark.imperics_shareddir
-def test_scale_covariate_change(monkeypatch):
+def test_scale_covariate_change():
     """Test the scale-covariate-changes option in EconomicCovariator and MeanWeatherCovariator.
 
     the scale-covariate-changes parameter is a scalar that rescales the fractional change of a covariate between a base year and a future year, and this is performed 
