7cfce3f5ef477b25778bbf62895ee27f250d2c80
Emile Tenezakis <etenezakis@battuta.berkeley.edu>
2021-06-02 21:51:04 -0700

    add pytest decorator

--- tests/test_agglib.py
+++ tests/test_agglib.py
@@ -1,6 +1,7 @@
 import pytest
 from generate import agglib
 
+@pytest.mark.imperics_shareddir
 def test_interpret_cost_use_args():
 
     assert agglib.interpret_cost_use_args(['clim_scenario', 'rcp_num'], 'whatever','/idontknow/rcp85/idk/idk/end','idk')[0]=='rcp85'
@@ -10,5 +11,5 @@ def test_interpret_cost_use_args():
     assert agglib.interpret_cost_use_args(['rcp_num', 'clim_scenario'], 'whatever','/idontknow/rcp85/idk/idk/end','idk')[0]=='85'
     # check seed reading works 
     assert agglib.interpret_cost_use_args(['seed-csvv'],
-     '/shares/gcp/outputs/agriculture/impacts-mealy/testing/montecarlo-cassava-261020/montecarlo',
+     'outputs/agriculture/impacts-mealy/testing/montecarlo-cassava-261020/montecarlo',
      'batch7/rcp45/surrogate_CanESM2_89/high/SSP3','cassava-031020.nc4')[0]==1603562142
