a52534a59ea9cc06a926a70bb08bc03f84138dbd
Emile Tenezakis <etenezakis@battuta.berkeley.edu>
2021-06-03 00:20:00 -0700

    fixed test_interpret_cost_args

--- tests/test_agglib.py
+++ tests/test_agglib.py
@@ -16,35 +16,39 @@ def test_interpret_cost_use_args():
      'batch7/rcp45/surrogate_CanESM2_89/high/SSP3','cassava-031020.nc4')[0]==1603562142
 
 @pytest.mark.imperics_shareddir
-def test_interpret_cost_args(self):
+def test_interpret_cost_args():
 
-	templates {'config' : {'outputdir':'outputs/agriculture/impacts-mealy/testing/montecarlo-cassava-261020/montecarlo'}, 
-	'targetdir' : 'batch7/rcp45/surrogate_CanESM2_89/high/SSP3'}
+	templates = {'config':{'outputdir':'outputs/agriculture/impacts-mealy/testing/montecarlo-cassava-261020/montecarlo'}, 
+	'targetdir' : 'batch7/rcp45/surrogate_CanESM2_89/high/SSP3',
+	'filename' : 'cassava-031020.nc4'}
 
 	strd = agglib.interpret_cost_args(costs_script={'extra-args': {'extra-arg1': 'extraarg1', 'extra-arg2': 'extraarg2'},
-	                                  'use-args': ['useargs1', 'useargs2']}, 
+	                                  'use-args': ['rcp_num', 'clim_scenario']}, 
 	                                  config=templates['config'], 
-	                                  targetdir=templates['targetdir'])
+	                                  targetdir=templates['targetdir'],
+	                                  filename=templates['filename'])
 
 	assert len(strd)==4
 	assert strd[0]=='extraarg1'
-	assert strd[3]=='useargs2'
+	assert strd[3]=='rcp45'
 
-	revert1 = agglib.interpret_cost_args(costs_script={'use-args': ['useargs1', 'useargs2'],
+	revert1 = agglib.interpret_cost_args(costs_script={'use-args': ['rcp_num', 'clim_scenario'],
                                   'extra-args': {'extra-arg1': 'extraarg1', 'extra-arg2': 'extraarg2'}}, 
                                   config=templates['config'], 
-	                              targetdir=templates['targetdir'])
+	                              targetdir=templates['targetdir'],
+	                              filename=templates['filename'])
 
-	assert conc[0]=='useargs1'
-	assert conc[3]=='extraarg2'
+	assert revert1[0]=='45'
+	assert revert1[3]=='extraarg2'
 
-	revert2 = agglib.interpret_cost_args(costs_script={'use-args': ['useargs2','useargs1'],
+	revert2 = agglib.interpret_cost_args(costs_script={'use-args': ['clim_scenario', 'rcp_num'],
                                   'extra-args': {'extra-arg1': 'extraarg2', 'extra-arg2': 'extraarg1'}}, 
                                   config=templates['config'], 
-	                              targetdir=templates['targetdir'])
+	                              targetdir=templates['targetdir'],
+	                              filename=templates['filename'])
 
-	assert conc[0]=='useargs2'
-	assert conc[3]=='extraarg1'
+	assert revert2[0]=='rcp45'
+	assert revert2[3]=='extraarg1'
 
 	with pytest.raises(ValueError):
 		agglib.interpret_cost_args(costs_script={'random-args':'idk'})
