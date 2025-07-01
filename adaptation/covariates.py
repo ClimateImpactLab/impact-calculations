36439e59859710225788e8af80ca2049088b052a
James Rising <jarising@gmail.com>
2025-07-01 01:19:16 -0500

    drop IR in code

--- adaptation/covariates.py
+++ adaptation/covariates.py
@@ -1388,6 +1388,7 @@ class GlobalAggregatedCovariator(Covariator):
         self.year_of_cache = None
         self.byregion_cache = None # {region: { key-local: value }}
         self.global_cache = None # {key: value }
+        self.keep_local = ['ir-share']
 
     def get_current(self, region):
         """
@@ -1400,7 +1401,9 @@ class GlobalAggregatedCovariator(Covariator):
         dict
         """
         if self.byregion_cache:
-            return {**{(key + '-local'): self.byregion_cache[region][key] for key in self.byregion_cache[region]}, **self.global_cache}
+            return {**{(key + '-local'): self.byregion_cache[region][key] for key in self.byregion_cache[region] if key not in self.keep_local},
+                    **{key: self.byregion_cache[region][key] for key in self.byregion_cache[region] if key in self.keep_local},
+                    **self.global_cache}
 
         self.byregion_cache = {region: self.source.get_current(region) for region in self.regions}
 
@@ -1408,6 +1411,8 @@ class GlobalAggregatedCovariator(Covariator):
 
         self.global_cache = {}
         for key in self.byregion_cache[list(self.regions)[0]]:
+            if key in self.keep_local:
+                continue
             regionvalues = np.array([self.byregion_cache[region][key] for region in self.regions])
             indices = ~np.isnan(regionvalues)
             self.global_cache[key] = np.average(regionvalues[indices], weights=pop_baseline[indices])
